9b92ac1690d81694ceb99522b78ae8c433261304
Gethi <bigard.antoine@gmail.com>
2020-11-09 11:34:44 +0100

    Replace Template by catalogue apprentissage

--- ui/src/pages/Layout.js
+++ ui/src/pages/Layout.js
@@ -14,7 +14,7 @@ export default (props) => {
   return (
     <Site>
       <Site.Header>
-        Template App
+        Catalogue Apprentissage App
         <div className="d-flex order-lg-2 ml-auto">
           <Nav.Item hasSubNav value={auth.sub} icon="user">
             <a className="dropdown-item" onClick={logout}>
