5009016513e00318823a9067934230f4cbd18ae0
Gethi <bigard.antoine@gmail.com>
2020-11-09 13:25:28 +0100

    chore: dummy change

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -55,7 +55,7 @@ module.exports = async (components) => {
         });
 
       return res.json({
-        name: `Serveur MNA - ${config.appName}`,
+        name: `Serveur Express MNA - ${config.appName}`,
         version: packageJson.version,
         env: config.env,
         healthcheck: {
