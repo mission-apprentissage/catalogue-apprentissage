158fb0ae9ac479ebf260469022421a5f1f7f9a6b
Kevin Barnoin <k.barnoin@gmail.com>
2020-11-25 18:17:25 +0100

    add importer standalone job

---
+++ server/src/jobs/psCoverage/importer/psImporter.js
@@ -0,0 +1,62 @@
+// #region Imports
+const logger = require("../../../common/logger");
+
+const { PsFormation } = require("../../../common/model");
+const { asyncForEach } = require("../../../common/utils/asyncUtils");
+const { getJsonFromXlsxFile } = require("../../../common/utils/fileUtils");
+
+const { runScript } = require("../../scriptWrapper");
+
+const run = async ({ tco }) => {
+  try {
+    const data = getJsonFromXlsxFile("FILEPATH");
+
+    asyncForEach(data, async (formation) => {
+      if (!formation.code_cfd && formation.code_mef_10) {
+        const responseMEF = await tco.getMefInfo(formation.code_mef_10);
+        if (responseMEF) {
+          if (!responseMEF.messages.cfdUpdated === "Non trouvé") {
+            formation.code_cfd = responseMEF.result.cfd.cfd;
+          }
+        }
+      }
+
+      logger.info(`Add ${formation.LIB_AFF} — ${formation.CODEMEF} to DB`);
+      await PsFormation.create({
+        uai_gestionnaire: formation.UAI_GES,
+        uai_composante: formation.UAI_COMPOSANTE,
+        libelle_uai_composante: formation.LIB_COMPOSANTE,
+        uai_affilie: formation.UAI_AFF,
+        libelle_uai_affilie: formation.LIB_AFF,
+        code_commune_insee: formation.CODECOMMUNE,
+        libelle_commune: formation.LIBCOMMUNE,
+        code_postal: formation.CODEPOSTAL,
+        nom_academie: formation.ACADEMIE,
+        code_ministere: formation.MINISTERETUTELLE,
+        libelle_ministere: formation.LIBMINISTERE,
+        type_etablissement: formation.TYPEETA,
+        code_formation: formation.CODEFORMATION,
+        libelle_formation: formation.LIBFORMATION,
+        code_specialite: formation.CODESPECIALITE,
+        libelle_specialite: formation.LIBSPECIALITE,
+        code_formation_initiale: formation.CODESPEFORMATIONINITIALE,
+        code_mef_10: formation.CODEMEF,
+        code_cfd: formation.CODECFD,
+        code_cfd_2: formation.CODECFD2,
+        code_cfd_3: formation.CODECFD3,
+      });
+    });
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+if (process.env.standalone) {
+  runScript(async ({ tco }) => {
+    logger.info(" -- Start database import -- ");
+
+    await run(tco);
+
+    logger.info(" -- End database import -- ");
+  });
+}
