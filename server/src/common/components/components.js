669b1f11a9147a5fb9a626b703256677016c5764
Kevin Barnoin <k.barnoin@gmail.com>
2020-11-25 18:14:44 +0100

    add table de correspondance component

--- server/src/common/components/components.js
+++ server/src/common/components/components.js
@@ -3,9 +3,11 @@ const createUsers = require("./users");
 
 module.exports = async (options = {}) => {
   const users = options.users || (await createUsers());
+  const tco = options.tco;
 
   return {
     users,
+    tco,
     db: options.db || (await connectToMongo()).db,
   };
 };
