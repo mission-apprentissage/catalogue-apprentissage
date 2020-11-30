8556c9128575096abbf6c5c8c7a90e942e4aa804
Eric <eric.plaquevent@gmail.com>
2020-11-30 14:00:04 +0100

    feat: Add pSup loader to server jobs

--- server/src/jobs/index.js
+++ server/src/jobs/index.js
@@ -2,12 +2,14 @@ const { runScript } = require("./scriptWrapper");
 const logger = require("../common/logger");
 const rcoImporter = require("./rcoImporter");
 const rcoConverter = require("./rcoConverter");
+const pSupLoader = require("./oneshot/pSupTemporary");
 
 runScript(async () => {
   try {
     logger.info(`Start all jobs`);
     await rcoImporter();
     await rcoConverter();
+    await pSupLoader();
   } catch (error) {
     logger.error(error);
   }
