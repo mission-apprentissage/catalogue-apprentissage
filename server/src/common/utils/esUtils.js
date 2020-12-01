2bdf33c8b22a1b57298cdce0452f7eaa4fbc1ad6
Gethi <bigard.antoine@gmail.com>
2020-12-01 14:47:17 +0100

    fix: mongoosatic

--- server/src/common/utils/esUtils.js
+++ server/src/common/utils/esUtils.js
@@ -7,6 +7,10 @@ const rebuildIndex = async (index, schema) => {
   logger.info(`Removing '${index}' index...`);
   await client.indices.delete({ index });
 
+  logger.info(`Re-creating '${index}' index with mapping...`);
+  let requireAsciiFolding = true;
+  await schema.createMapping(requireAsciiFolding); // this explicit call of createMapping insures that the geo points fields will be treated accordingly during indexing
+
   logger.info(`Synching '${index}' index ...`);
   await schema.synchronize();
 };
