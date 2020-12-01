ad0480f3811781731df56e1ccb87c113ee8fcd28
Kevin Barnoin <k.barnoin@gmail.com>
2020-12-01 17:15:44 +0100

    crud coverage (create / delete) wip

---
+++ server/src/jobs/psCoverage/crudCoverage.js
@@ -0,0 +1,52 @@
+const logger = require("../../common/logger");
+const path = require("path");
+const diff = require("deep-object-diff").diff;
+
+const { PsFormation } = require("../../common/model/index");
+const { asyncForEach } = require("../../common/utils/asyncUtils");
+const { getJsonFromXlsxFile } = require("../../common/utils/fileUtils");
+
+module.exports = async () => {
+  const filePath = path.resolve(__dirname, "./assets/Etablissements_6_20201130.xlsx");
+  let rawFormations = getJsonFromXlsxFile(filePath);
+
+  const toCreate = rawFormations.filter((x) => x.Analyse === "CREATE");
+  const toDelete = rawFormations.filter((x) => x.Analyse === "DELETE");
+  const ok = rawFormations.filter((x) => x.Analyse === "TRUE");
+  const standby = rawFormations.filter((x) => x.Analyse === "STANDBY");
+
+  console.log({
+    base: rawFormations.length,
+    create: toCreate.length,
+    delete: toDelete.length,
+    standby: standby.length,
+    ok: ok.length,
+  });
+
+  Promise.all(
+    toCreate.map(async (etab) => {
+      return;
+      const payload = {
+        uai: etab.uai_gestionnaire,
+        siret: etab.etablissement_siret,
+      };
+
+      await postEtablissement(payload)
+        .then(() => logger.info(`Etablissement ${payload.uai} successfully added`))
+        .catch((error) => logger.error(error));
+    })
+  );
+
+  Promise.all(
+    toDelete.map(async (etab) => {
+      return;
+      const id = etab.etablissement_id;
+
+      await deleteEtablissement(id)
+        .then(() => logger.info(`Etablissement ${id} has been deleted`))
+        .catch((error) => logger.error(error));
+    })
+  );
+
+  return;
+};
