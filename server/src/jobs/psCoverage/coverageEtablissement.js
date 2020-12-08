a443427ba8675f717208c3b11f3696985499cdc9
Kevin Barnoin <k.barnoin@gmail.com>
2020-12-08 09:51:56 +0100

    optim coverageEtablissement

--- server/src/jobs/psCoverage/coverageEtablissement.js
+++ server/src/jobs/psCoverage/coverageEtablissement.js
@@ -3,58 +3,67 @@ const { PsFormation } = require("../../common/model");
 const logger = require("../../common/logger");
 
 module.exports = async (catalogue) => {
+  logger.info("Get parcoursup formations...");
   const psFormation = await PsFormation.find({ matching_type: { $ne: null } }).lean();
+
+  logger.info("Get catalogue establishements...");
+  const baseEtablissement = await catalogue.getEtablissements();
+
   logger.info(`${psFormation.length} formation à traiter`);
 
-  await asyncForEach(psFormation, async (formation) => {
-    logger.info(`Processing ${formation.libelle_uai_affilie} — mef : ${formation.code_mef_10}`);
+  await asyncForEach(psFormation, async (formation, index) => {
+    logger.info(
+      `Processing ${index}/${PsFormation.length} - ${formation.libelle_uai_affilie} — mef : ${formation.code_mef_10}`
+    );
     let etablissements = [];
-
-    // TODO ? filter matching_mna_formation to avoid duplicate calls/entries
-    // const filter = formation.matching_mna_formation.reduce((acc, formation) => {});
 
     await asyncForEach(formation.matching_mna_formation, async (matches, index) => {
       logger.info(`Processing ${index + 1} of ${formation.matching_mna_formation.length} - cfd : ${matches.cfd}`);
 
-      // console.log({
-      //   "UAI FORMATION": matches.uai_formation,
-      //   "UAI GESTIONNAIRE": matches.etablissement_gestionnaire_uai,
-      //   "UAI FORMATEUR": matches.etablissement_formateur_uai,
-      // });
-
       if (matches.uai_formation) {
-        let resuai = await catalogue.getEtablissements({ query: { uai: matches.uai_formation } });
+        let resuai = baseEtablissement.filter((x) => x.uai === matches.uai_formation);
+
         if (resuai.length > 0) {
           logger.info(`Found ${resuai.length} matches with UAI_FORMATION`);
-          etablissements.push({ data: resuai, matched_uai: "UAI_FORMATION" });
+          resuai.forEach((x) => etablissements.push({ ...x, matched_uai: "UAI_FORMATION" }));
         }
       }
 
       if (matches.etablissement_formateur_uai) {
-        let resformateur = await catalogue.getEtablissements({ query: { uai: matches.etablissement_formateur_uai } });
+        let resformateur = baseEtablissement.filter((x) => x.uai === matches.etablissement_formateur_uai);
 
         if (resformateur.length > 0) {
           logger.info(`Found ${resformateur.length} matches with UAI_FORMATEUR`);
-          etablissements.push({ data: resformateur, matched_uai: "UAI_FORMATEUR" });
+          resformateur.forEach((x) => etablissements.push({ ...x, matched_uai: "UAI_FORMATEUR" }));
         }
       }
 
       if (matches.etablissement_gestionnaire_uai) {
-        let resgestionnaire = await catalogue.getEtablissements({
-          query: { uai: matches.etablissement_gestionnaire_uai },
-        });
+        let resgestionnaire = baseEtablissement.filter((x) => x.uai === matches.etablissement_gestionnaire_uai);
 
         if (resgestionnaire.length > 0) {
           logger.info(`Found ${resgestionnaire.length} matches with UAI_GESTIONNAIRE`);
-          etablissements.push({ data: resgestionnaire, matched_uai: "UAI_GESTIONNAIRE" });
+          resgestionnaire.forEach((x) => etablissements.push({ ...x, matched_uai: "UAI_GESTIONNAIRE" }));
         }
       }
     });
 
     if (etablissements.length === 0) return;
 
+    const t = etablissements.map((x) => {
+      x.id_mna_etablissement = x._id;
+      delete x._id;
+      delete x.__v;
+      return x;
+      // await PsFormation.findByIdAndUpdate(formation._id, {
+      //   $push: {
+      //     matching_mna_etablissement: etablissements,
+      //   },
+      // });
+    });
+
     await PsFormation.findByIdAndUpdate(formation._id, {
-      matching_mna_etablissement: etablissements,
+      matching_mna_etablissement: t,
     });
   });
 };
