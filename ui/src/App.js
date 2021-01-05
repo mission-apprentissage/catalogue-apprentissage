2c2d0c7395def3f0dbc5c3ddb5f172b36a8d4b7e
Gethi <bigard.antoine@gmail.com>
2021-01-05 09:48:56 +0100

    fix: re-add webfont TEST

--- ui/src/App.js
+++ ui/src/App.js
@@ -20,6 +20,7 @@ import HowToModif from "./pages/HowToModif";
 import { _post } from "./common/httpClient";
 import ScrollToTop from "./common/components/ScrollToTop";
 import Layout from "./pages/layout/Layout";
+import WebFont from "webfontloader";
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -35,7 +36,13 @@ function PrivateRoute({ children, ...rest }) {
     />
   );
 }
-
+WebFont.load({
+  google: { families: ["Josefin Sans: 200, 300, 400, 600, 700", "Nexa", "Raleway"] },
+  custom: {
+    families: ["Font Awesome 5 Icons:400,900", "Font Awesome 5 Brands:400"],
+    urls: ["//use.fontawesome.com/releases/v5.0.8/css/all.css"],
+  },
+});
 const queryClient = new QueryClient();
 
 export default () => {
