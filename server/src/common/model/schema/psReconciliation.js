6e5ef5a77ed6c7c9902c853828c7fbf459ed0b81
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-06 10:00:24 +0100

    feat: update model

--- server/src/common/model/schema/psReconciliation.js
+++ server/src/common/model/schema/psReconciliation.js
@@ -1,44 +1,44 @@
 const psReconciliationSchema = {
-  etablissement: [
-    {
-      id: {
-        type: String,
-        default: null,
-        description: "Identifiant de l'établissement catalogue lié à la formation parcoursup",
-      },
-      type: {
-        type: String,
-        default: null,
-        description: "Type d'établissement lié à la formation parcousup",
-      },
-    },
-  ],
+  uai_gestionnaire: {
+    type: String,
+    default: null,
+    description: "uai gestionnaire de la formation parcoursup",
+  },
+  uai_formateur: {
+    type: String,
+    default: null,
+    description: "uai formateur de la formation parcousup",
+  },
+  uai_affilie: {
+    type: String,
+    default: null,
+    description: "uai affilie de la formation parcoursup",
+  },
+  siret_formateur: {
+    type: String,
+    default: null,
+    description: "siret de l'établissement formateur",
+  },
+  siret_gestionnaire: {
+    type: String,
+    default: null,
+    description: "siret de l'établissement gestionnaire",
+  },
+  siret_gestionnaire_formateur: {
+    type: String,
+    default: null,
+    description: "siret de l'établissement gestionnaire & formateur",
+  },
   code_cfd_formation: {
     type: String,
     default: null,
     description:
       "Code formation diplome de la formation catalogue rapproché. Valeur récupéré de la formation parcousup ou du catalogue des formations pour les matching 1*",
   },
-  code_postal_formation: {
-    type: String,
-    default: null,
-    description:
-      "Code postal de la formation catalogue rapproché. Valeur récupéré de la formation parcousup ou du catalogue des formations pour les matching 1* ",
-  },
-  id_formation_mna: {
-    type: String,
-    default: null,
-    description: "Id de la formation catalogue",
-  },
-  id_formation_ps: {
+  id_psformation: {
     type: String,
     default: null,
     description: "Id de la formation parcoursup",
-  },
-  annee_formation: {
-    type: String,
-    default: null,
-    description: "Année de la formation parcousup",
   },
 };
 
