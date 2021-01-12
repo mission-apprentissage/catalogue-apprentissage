0fce0ba1525b97be16a962027de0bf1e04e94440
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-13 00:20:09 +0100

    fix: clean code + todo

--- server/src/jobs/oneshot/psFormation/psReconciliation.js
+++ server/src/jobs/oneshot/psFormation/psReconciliation.js
@@ -31,6 +31,9 @@ module.exports = async (catalogue, filePath) => {
 
   let result = match.map((item) => combinate(item));
   console.log("result", result); // husky wip
+  /**
+   * TODO : save in DB
+   */
 };
 
 /**
@@ -40,22 +43,17 @@ module.exports = async (catalogue, filePath) => {
 function distribute(arr1, arr2) {
   let ratio = Math.ceil(size(arr1) / size(arr2));
   let pattern = chain(new Array(ratio)).fill(arr2).flatten().take(size(arr1)).value();
+
   return zip(arr1, pattern);
 }
 
 function combinate(array) {
-  if (size(array) === 1) {
+  if (size(array) <= 2) {
     return array;
   }
-
-  if (size(array) === 2) {
-    return array;
-  }
-
-  let set = partition(array, (o) => o.type === "formateur");
-  let [formateur, gestionnaire] = set;
 
   let result = [];
+  let [formateur, gestionnaire] = partition(array, (x) => x.type === "formateur");
 
   if (size(formateur) > size(gestionnaire)) {
     result.push(distribute(formateur, gestionnaire));
