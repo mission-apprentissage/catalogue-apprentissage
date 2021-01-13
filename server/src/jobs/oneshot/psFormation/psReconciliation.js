d73548389e2d793d5894678ed02fe51d84cbf298
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-13 10:45:25 +0100

    feat: generate etab & save in DB

--- server/src/jobs/oneshot/psFormation/psReconciliation.js
+++ server/src/jobs/oneshot/psFormation/psReconciliation.js
@@ -1,11 +1,14 @@
 const { getJsonFromXlsxFile } = require("../../../common/utils/fileUtils");
 const logger = require("../../../common/logger");
 const { partition, zip, size, chain } = require("lodash");
+const { asyncForEach } = require("../../../common/utils/asyncUtils");
+const { PsReconciliation } = require("../../../common/model");
 
 module.exports = async (catalogue, filePath) => {
   logger.info(`Traitement du fichier ${filePath} ...`);
   let data = getJsonFromXlsxFile(filePath);
 
+  // format dataset per formation_id
   const match = Object.values(
     data
       .filter((x) => x.Analyse === "CREATE" || x.Analyse === "TRUE")
@@ -29,11 +32,35 @@ module.exports = async (catalogue, filePath) => {
       }, [])
   );
 
+  // generate all possible combination between formateur and gestionnaire
   let result = match.map((item) => combinate(item));
-  console.log("result", result); // husky wip
-  /**
-   * TODO : save in DB
-   */
+
+  // create establishments
+  await asyncForEach(result, async (item) => {
+    if (item.Analyse === "CREATE") {
+      await catalogue.createEtablissement({ uai: item.uai_gestionnaire, siret: item.etablissement_siret });
+    }
+  });
+
+  // save in db
+  await asyncForEach(result, async (couple) => {
+    const payload = couple.reduce((acc, item) => {
+      acc.uai_gestionnaire = item.uai_gestionnaire;
+      acc.uai_affilie = item.uai_gestionnaire;
+      acc.uai_composante = item.uai_gestionnaire;
+      acc.code_cfd = item.code_cfd;
+      acc.siret_formateur = item.type === "formateur" ? item.etablissement_siret : acc.siret_formateur;
+      acc.siret_gestionnaire = item.type === "gestionnaire" ? item.etablissement_siret : acc.siret_gestionnaire;
+
+      return acc;
+    }, {});
+
+    try {
+      await PsReconciliation.save(payload);
+    } catch (error) {
+      logger.error(error);
+    }
+  });
 };
 
 /**
