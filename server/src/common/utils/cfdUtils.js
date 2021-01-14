14bf03ca073255196c14259881f629581f15e196
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-14 17:23:57 +0100

    feat: update mapper

---
+++ server/src/common/utils/cfdUtils.js
@@ -0,0 +1,5 @@
+module.exports = (cfd) => {
+  if (!cfd || !/^[0-9A-Z]{8}[A-Z]?$/g.test(cfd.trim())) {
+    return false;
+  }
+};
--- server/src/logic/mappers/psReferenceMapper.js
+++ server/src/logic/mappers/psReferenceMapper.js
@@ -4,25 +4,71 @@
 
 const logger = require("../../common/logger");
 const { PsReconciliation } = require("../../common/model");
+const checkCFD = require("../../common/utils/cfdUtils");
+const checkSIRET = require("../../common/utils/siretUtils");
 
 module.exports = async (payload) => {
-  const { cfd, siret } = payload;
+  const { cfd, siret_formateur, siret_gestionnaire } = payload;
 
-  if (!cfd || !siret) {
-    throw new Error(`cfd and siret are required`);
+  if (checkCFD(cfd) === false) {
+    return {
+      parcoursup_reference: false,
+      messages: {
+        error: "Le code formation diplôme doit être définit et au format 8 caractères ou 9 avec la lettre specialité",
+      },
+    };
+  }
+
+  if (checkSIRET(siret_formateur) === false) {
+    return {
+      parcoursup_reference: false,
+      messages: {
+        error: "Le siret formateur doit être définit et au format 14 caractères",
+      },
+    };
+  }
+
+  if (checkSIRET(siret_gestionnaire) === false) {
+    return {
+      parcoursup_reference: false,
+      messages: {
+        error: "Le siret gestionnaire doit être au format 14 caractères",
+      },
+    };
   }
 
   try {
-    const response = await PsReconciliation.find({
+    const response = await PsReconciliation.findOne({
       code_cfd: cfd,
-      $and: [
-        {
-          $or: [{ siret_formateur: siret }, { siret_gestionnaire: siret }],
-        },
-      ],
+      siret_formateur: siret_formateur,
+      siret_gestionnaire: siret_gestionnaire,
     });
-    return response;
+
+    if (!response) {
+      const checkReverse = await PsReconciliation.findOne({
+        code_cfd: cfd,
+        siret_formateur: siret_gestionnaire,
+        siret_gestionnaire: siret_gestionnaire,
+      });
+
+      if (!checkReverse) {
+        return {
+          parcoursup_reference: false,
+          message: "Les sirets sont inversés",
+        };
+      }
+
+      return {
+        parcoursup_reference: false,
+      };
+    }
+
+    return {
+      parcoursup_reference: true,
+    };
   } catch (error) {
     logger.error(error);
+
+    return {};
   }
 };
