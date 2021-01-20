90e8789b743d4aaac758f8df927455f895936064
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-20 17:26:06 +0100

    feat: ui affelnet

---
+++ ui/src/pages/reconciliation-affelnet/components/index.js
@@ -0,0 +1,6 @@
+import Layout from "./Layout";
+import Accordion from "./Accordion";
+import Loading from "./Loading";
+import addEtablissement from "./addEtablissement";
+
+export { Layout, Accordion, Loading, addEtablissement };
