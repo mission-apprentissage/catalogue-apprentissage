b93cade93d4b82c64953669028806327d71938bc
Gethi <bigard.antoine@gmail.com>
2021-01-21 12:26:14 +0100

    chore: remove commented lines

--- ui/src/pages/Search/components/CardListFormation/CardList.js
+++ ui/src/pages/Search/components/CardListFormation/CardList.js
@@ -1,47 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faTimes } from "@fortawesome/free-solid-svg-icons"; // faPen
 import { Badge } from "@chakra-ui/react";
-// import { API } from "aws-amplify";
 import "./cardList.css";
 import { hasOneOfRoles } from "../../../../common/utils/rolesUtils";
 import useAuth from "../../../../common/hooks/useAuth";
 
-//import image_preview from "./noimage.png";
-
-// const checkIfHasRightToEdit = (item, userAcm) => {
-//   let hasRightToEdit = userAcm.all;
-//   if (!hasRightToEdit) {
-//     hasRightToEdit = userAcm.academie.includes(`${item.num_academie}`);
-//   }
-//   return hasRightToEdit;
-// };
-
 const CardList = ({ data, f2021 }) => {
   let [auth] = useAuth();
-  // const { acm: userAcm } = useSelector((state) => state.user);
-  //const ImageComponent = <img src={image_preview} alt={data.intitule_court} />;
-
-  //const hasRightToEdit = false; // checkIfHasRightToEdit(data, userAcm);
-
-  // const onDeleteClicked = async (e) => {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  //   // eslint-disable-next-line no-restricted-globals
-  //   const areYousure = confirm("Souhaitez-vous vraiment supprimer cette formation ?");
-  //   if (areYousure) {
-  //     // await API.del("api", `/formation/${data._id}`);
-  //     window.location.reload();
-  //   }
-  // };
-  // const onEditClicked = async e => {
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  //   // DO STUFF
-  // };
 
   const niv = data.niveau.replace(/\D/g, "");
 
@@ -84,39 +50,3 @@ const CardList = ({ data, f2021 }) => {
 };
 
 export default CardList;
-
-/*
-
-<div className="thumbnail">
-          <div className="field">
-            <p>Niveau: {data.niveau}</p>
-          </div>
-          <div className="field">
-            <p>Code diplôme: {data.cfd}</p>
-          </div>
-        </div>
-        <div className="content">
-          <div style={{ display: "flex" }}>
-            <h2>
-              {data.intitule_long}
-              <br />
-              <small>{data.diplome}</small>
-            </h2>
-            <span className="edition-btns">
-              {hasRightToEdit && (
-                <>
-                  <Button variant="outline" colorScheme="red" onClick={onDeleteClicked}>
-                    <FontAwesomeIcon icon={faTimes} />
-                  </Button>
-                </>
-              )}
-            </span>
-          </div>
-          <div>
-            <p>{data.nom_academie}</p>
-            <p>{data.code_postal}</p>
-            <p>{data.etablissement_gestionnaire_entreprise_raison_sociale}</p>
-            <p>{data.etablissement_formateur_enseigne}</p>
-          </div>
-        </div>
-*/
