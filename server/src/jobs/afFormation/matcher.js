0036b192fe523a8c121ec4cdf843ed24f836d240
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-25 12:27:56 +0100

    fix: remove dead code

--- server/src/jobs/afFormation/matcher.js
+++ server/src/jobs/afFormation/matcher.js
@@ -1,18 +1,6 @@
 const { formation } = require("./mapper");
-// const { capitalize } = require("lodash");
 
-function generateMatching(mode, matchType, ligne, catalogue) {
-  // const uai = (catalogue) =>
-  //   catalogue.uai_formation === ligne.uai ||
-  //   catalogue.etablissement_formateur_uai === ligne.uai ||
-  //   catalogue.etablissement_gestionnaire_uai === ligne.uai;
-
-  // const academieFormated = ligne.academie
-  //   .split("-")
-  //   .filter((x) => x)
-  //   .map((x) => capitalize(x))
-  //   .join("-");
-
+function generateMatching(matchType, ligne, catalogue) {
   const cp = (catalogue) =>
     `${catalogue.etablissement_gestionnaire_code_postal}` === `${ligne.code_postal_modified.code_postal}` ||
     `${catalogue.code_postal}` === `${ligne.code_postal_modified.code_postal}` ||
@@ -21,14 +9,7 @@ function generateMatching(mode, matchType, ligne, catalogue) {
   const dept = (catalogue) =>
     `${catalogue.num_departement}` === `${ligne.code_postal_modified.code_postal.substring(0, 2)}`;
 
-  // const academie = (catalogue) =>
-  //   ligne.code_postal_modified
-  //     ? `${catalogue.num_academie}` === `${ligne.code_postal_modified.num_academie}`
-  //     : catalogue.nom_academie === academieFormated;
-
   const cfd = (catalogue) => catalogue.cfd === ligne.code_cfd;
-
-  // const libelle = (catalogue) => ligne.libelle_ban.includes(catalogue.intitule_court);
 
   const filter = (condition) => {
     let result = catalogue.filter((catalogue) => condition(catalogue));
@@ -38,22 +19,6 @@ function generateMatching(mode, matchType, ligne, catalogue) {
     };
   };
 
-  // if (mode === "UAI") {
-  //   switch (matchType) {
-  //     case "1":
-  //       return filter((i) => uai(i));
-  //     case "2":
-  //       return filter((i) => uai(i) && cfd(i));
-  //     case "3":
-  //       return filter((i) => uai(i) && cfd(i) && cp(i));
-  //     case "4":
-  //       return filter((i) => uai(i) && cfd(i) && cp(i) && academie(i));
-  //     case "5":
-  //       return filter((i) => uai(i) && cfd(i) && cp(i) && academie(i) && libelle(i));
-  //     default:
-  //       break;
-  //   }
-  // } else {
   switch (matchType) {
     case "1":
       return filter((i) => cfd(i));
@@ -64,21 +29,19 @@ function generateMatching(mode, matchType, ligne, catalogue) {
     default:
       break;
   }
-  // }
 }
-
-let predicateCFD = [
-  (ligne, catalogue) => generateMatching("CFD", "3", ligne, catalogue),
-  (ligne, catalogue) => generateMatching("CFD", "2", ligne, catalogue),
-  (ligne, catalogue) => generateMatching("CFD", "1", ligne, catalogue),
-];
 
 module.exports = (ligne, catalogue) => {
   let buffer = {
     formation: ligne,
-    // matching_uai: [],
     matching_cfd: [],
   };
+
+  let predicateCFD = [
+    (ligne, catalogue) => generateMatching("3", ligne, catalogue),
+    (ligne, catalogue) => generateMatching("2", ligne, catalogue),
+    (ligne, catalogue) => generateMatching("1", ligne, catalogue),
+  ];
 
   for (let i = 0; i < predicateCFD.length; i++) {
     let predicate = predicateCFD[i];
