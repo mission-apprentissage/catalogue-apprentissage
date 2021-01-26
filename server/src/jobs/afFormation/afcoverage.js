64b1d615a4ba22541cd775e99a870dd178bc40d5
Kevin Barns <k.barnoin@gmail.com>
2021-01-26 13:32:27 +0100

    Develop (#109)
    * fix: link path + affelnet (#97)
    
    * Fix: update coverage (fix, refacto & clean up) (#98)
    
    * fix: reconciliation table & route & ui
    
    * fix: clean code
    
    * fix: clean code
    
    * fix: update route
    
    * fix: remove dead code
    
    * fix(importer): save some memory in rco:importer (#99)
    
    * feat: mapper (#100)
    
    * fix: enum in mongoose schema
    
    thanks @kevbarns
    
    * fix: coverage state + script (#101)
    
    * feat: reconciliation script update
    
    * feat: models
    
    * feat: sorting by non-reconciliated
    
    * fix: add total page number
    
    * feat: ui adaptation
    
    * fix: post route
    
    * fix: missing id
    
    * feat: Affelnet reference & pertinence script (#104)
    
    * Fix: affelnet importer (#105)
    
    * fix: importer
    
    * fix: add lean()
    
    * Fix: affelnet coverage memory leak (#108)
    
    * fix: memory leak (wip)
    
    * refacto
    
    Co-authored-by: Eric Plaquevent <eric.plaquevent@gmail.com>

--- server/src/jobs/afFormation/afcoverage.js
+++ server/src/jobs/afFormation/afcoverage.js
@@ -1,12 +1,10 @@
-const { asyncForEach } = require("../../common/utils/asyncUtils");
 const { AfFormation, ConvertedFormation } = require("../../common/model");
+const { oleoduc, writeData } = require("oleoduc");
 const logger = require("../../common/logger");
-const matcher = require("./matcher");
+const { formation } = require("./mapper");
 
-async function updateMatchedFormation({ formation, matching_cfd }) {
-  if (matching_cfd.length === 0) return;
-
-  let { matching_strengh, data } = matching_cfd[0];
+const updateMatchedFormation = async ({ formation, matching_cfd }) => {
+  let { matching_strengh, data } = matching_cfd;
 
   logger.info(`update ${formation._id} — strengh: ${matching_strengh}`);
 
@@ -14,33 +12,74 @@ async function updateMatchedFormation({ formation, matching_cfd }) {
     matching_type: matching_strengh,
     matching_mna_formation: data,
   });
-}
+};
+
+const formatResult = (strengh, data) => {
+  return {
+    matching_strengh: strengh,
+    data_length: data.length,
+    data: formation(data),
+  };
+};
+
+const match1 = (cfd) => ConvertedFormation.find({ cfd });
+const match2 = (cfd, num_departement) => ConvertedFormation.find({ cfd, num_departement });
+const match3 = (cfd, num_departement, code_postal) =>
+  ConvertedFormation.find({
+    cfd,
+    num_departement,
+    $and: [
+      {
+        $or: [
+          { etablissement_formateur_code_postal: code_postal },
+          { etablissement_gestionnaire_code_postal: code_postal },
+          { code_postal },
+        ],
+      },
+    ],
+  });
 
 module.exports = async (tableCorrespondance) => {
-  logger.info("Retreiving data from DB ...");
+  await oleoduc(
+    AfFormation.find({ code_cfd: { $ne: null } })
+      .lean()
+      .cursor(),
+    writeData(
+      async (formation) => {
+        const { messages, result } = await tableCorrespondance.getCpInfo(formation.code_postal);
 
-  const [afFormations, mnaFormations] = await Promise.all([
-    AfFormation.find({ code_cfd: { $ne: null } }).lean(),
-    ConvertedFormation.find().lean(),
-  ]);
+        if (
+          messages?.cp === "Ok" ||
+          messages?.cp === `Update: Le code ${formation.code_postal} est un code commune insee`
+        ) {
+          formation.code_postal = result.code_postal;
+        }
 
-  logger.info(`${afFormations.length} formations to update ...`);
+        let { code_cfd, code_postal } = formation;
+        let dept = code_postal.substring(0, 2);
 
-  await asyncForEach(afFormations, async (formation) => {
-    const { messages, result: resultTco } = await tableCorrespondance.getCpInfo(formation.code_postal);
+        const m3 = await match3(code_cfd, dept, code_postal);
 
-    if (
-      messages?.cp === "Ok" ||
-      messages?.cp === `Update: Le code ${formation.code_postal} est un code commune insee`
-    ) {
-      formation.code_postal_modified = { ...resultTco };
-    } else {
-      formation.code_postal_modified = { code_postal: formation.code_postal };
-    }
+        if (m3.length === 1) {
+          let matching_cfd = formatResult("3", m3);
+          await updateMatchedFormation({ formation, matching_cfd });
+        } else {
+          const m2 = await match2(code_cfd, dept);
 
-    const result = matcher(formation, mnaFormations);
-    logger.info(`Update ${formation._id} - ${formation.code_cfd} `);
+          if (m2.length === 1) {
+            let matching_cfd = formatResult("2", m2);
+            await updateMatchedFormation({ formation, matching_cfd });
+          } else {
+            const m1 = await match1(code_cfd);
 
-    await updateMatchedFormation(result);
-  });
+            if (m1.length > 0) {
+              let matching_cfd = formatResult("1", m1);
+              await updateMatchedFormation({ formation, matching_cfd });
+            }
+          }
+        }
+      },
+      { parallel: 5 }
+    )
+  );
 };
