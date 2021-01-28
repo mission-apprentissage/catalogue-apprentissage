258172724a966285ebe5ad9f6a1e846ccaa332dc
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-28 13:27:49 +0100

    refacto: remove bulk fetch

--- server/src/jobs/afFormation/afcoverageEtablissement.js
+++ server/src/jobs/afFormation/afcoverageEtablissement.js
@@ -8,11 +8,7 @@ const mongoose = require("mongoose");
 module.exports = async (catalogue) => {
   logger.info(`--- START ETABLISSEMENT COVERAGE ---`);
 
-  //const getEtablissements = (query) => catalogue.getEtablissements({ query });
-
-  const allEtablissements = await catalogue.getEtablissements({});
-
-  console.log("allEtablissements.length", allEtablissements.length);
+  const getEtablissements = (query) => catalogue.getEtablissements({ query });
 
   await oleoduc(
     AfFormation.find({ matching_type: { $ne: null } })
@@ -38,8 +34,7 @@ module.exports = async (catalogue) => {
             if (exist) return;
 
             if (uai_formation) {
-              // let resuai = await getEtablissements({ uai: uai_formation });
-              let resuai = allEtablissements.filter((x) => (x.uai = uai_formation));
+              let resuai = await getEtablissements({ uai: uai_formation });
 
               if (resuai.length > 0) {
                 resuai.forEach((x) => {
@@ -50,8 +45,7 @@ module.exports = async (catalogue) => {
             }
 
             if (etablissement_formateur_uai) {
-              // let resformateur = await getEtablissements({ uai: etablissement_formateur_uai });
-              let resformateur = allEtablissements.filter((x) => (x.uai = etablissement_formateur_uai));
+              let resformateur = await getEtablissements({ uai: etablissement_formateur_uai });
 
               if (resformateur.length > 0) {
                 resformateur.forEach((x) => {
@@ -62,8 +56,7 @@ module.exports = async (catalogue) => {
             }
 
             if (etablissement_gestionnaire_uai) {
-              // let resgestionnaire = await getEtablissements({ uai: etablissement_gestionnaire_uai });
-              let resgestionnaire = allEtablissements.filter((x) => (x.uai = etablissement_gestionnaire_uai));
+              let resgestionnaire = await getEtablissements({ uai: etablissement_gestionnaire_uai });
 
               if (resgestionnaire.length > 0) {
                 resgestionnaire.forEach((x) => {
