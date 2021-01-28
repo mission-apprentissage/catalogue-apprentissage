1b84639435df754ebfb419914bfd5e5f191b4941
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-01-28 14:16:59 +0100

    feat(affelnet): adjust affelnet status rules

--- server/src/jobs/common/utils/referenceUtils.js
+++ server/src/jobs/common/utils/referenceUtils.js
@@ -5,23 +5,6 @@ const toBePublishedRules = [
   { intitule_long: { $ne: "" } },
   { intitule_court: { $ne: null } },
   { intitule_court: { $ne: "" } },
-  {
-    $or: [
-      {
-        $or: [
-          { etablissement_formateur_conventionne: "OUI" },
-          {
-            etablissement_reference_declare_prefecture: "OUI",
-            etablissement_reference_datadock: "datadocké",
-          },
-        ],
-      },
-      {
-        rncp_eligible_apprentissage: true,
-        $or: [{ rncp_etablissement_formateur_habilite: true }, { rncp_etablissement_gestionnaire_habilite: true }],
-      },
-    ],
-  },
 ];
 
 module.exports = { toBePublishedRules };
