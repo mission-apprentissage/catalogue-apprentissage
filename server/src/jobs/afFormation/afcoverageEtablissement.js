21626f949a434201cc013acdf6aef3c7cd97c82e
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-28 15:49:31 +0100

    feat: paginator

--- server/src/jobs/afFormation/afcoverageEtablissement.js
+++ server/src/jobs/afFormation/afcoverageEtablissement.js
@@ -5,115 +5,91 @@ const { etablissement } = require("./mapper");
 // const { Readable } = require("stream");
 const mongoose = require("mongoose");
 const { asyncForEach } = require("../../common/utils/asyncUtils");
+const { paginator } = require("../common/utils/paginator");
 
 module.exports = async (catalogue) => {
   logger.info(`--- START ETABLISSEMENT COVERAGE ---`);
 
-  //const getEtablissements = (query) => catalogue.getEtablissements({ query });
+  const getEtablissements = (query) => catalogue.getEtablissements(query);
 
-  const allEtablissements = await catalogue.getEtablissements({});
+  await paginator(AfFormation, { lean: true, limit: 5 }, async ({ matching_mna_formation, _id }) => {
+    let etablissements = [];
 
-  console.log("allEtablissements.length", allEtablissements.length);
-
-  let offset = 0;
-  let limit = 5;
-  let computed = 0;
-  let nbFormations = 10;
-
-  while (computed < nbFormations) {
-    let { docs, total } = await AfFormation.paginate({}, { offset, limit });
-    nbFormations = total;
-
-    await Promise.all(
-      docs.map(async ({ matching_mna_formation, _id }) => {
-        let etablissements = [];
-
-        computed += 1;
-
-        await asyncForEach(
-          matching_mna_formation,
-          async ({ uai_formation, etablissement_formateur_uai, etablissement_gestionnaire_uai }) => {
-            let exist = etablissements.find(
-              (x) =>
-                x.uai === uai_formation ||
-                x.uai === etablissement_formateur_uai ||
-                x.uai === etablissement_gestionnaire_uai
-            );
-
-            if (exist) return;
-
-            if (uai_formation) {
-              // let resuai = await getEtablissements({ uai: uai_formation });
-              let resuai = allEtablissements.filter((x) => (x.uai = uai_formation));
-
-              if (resuai.length > 0) {
-                resuai.forEach((x) => {
-                  const formatted = etablissement(x);
-                  etablissements.push({ ...formatted, matched_uai: "UAI_FORMATION" });
-                });
-              }
-            }
-
-            if (etablissement_formateur_uai) {
-              // let resformateur = await getEtablissements({ uai: etablissement_formateur_uai });
-              let resformateur = allEtablissements.filter((x) => (x.uai = etablissement_formateur_uai));
-
-              if (resformateur.length > 0) {
-                resformateur.forEach((x) => {
-                  const formatted = etablissement(x);
-                  etablissements.push({ ...formatted, matched_uai: "UAI_FORMATEUR" });
-                });
-              }
-            }
-
-            if (etablissement_gestionnaire_uai) {
-              // let resgestionnaire = await getEtablissements({ uai: etablissement_gestionnaire_uai });
-              let resgestionnaire = allEtablissements.filter((x) => (x.uai = etablissement_gestionnaire_uai));
-
-              if (resgestionnaire.length > 0) {
-                resgestionnaire.forEach((x) => {
-                  const formatted = etablissement(x);
-                  etablissements.push({ ...formatted, matched_uai: "UAI_GESTIONNAIRE" });
-                });
-              }
-            }
-          }
+    await asyncForEach(
+      matching_mna_formation,
+      async ({ uai_formation, etablissement_formateur_uai, etablissement_gestionnaire_uai }) => {
+        let exist = etablissements.find(
+          (x) =>
+            x.uai === uai_formation || x.uai === etablissement_formateur_uai || x.uai === etablissement_gestionnaire_uai
         );
 
-        if (etablissements.length === 0) return;
+        if (exist) return;
 
-        const result = etablissements.reduce((acc, item) => {
-          if (!acc[item._id]) {
-            acc[item._id] = item;
-            acc[item._id].matched_uai = [acc[item._id].matched_uai];
-          } else {
-            const exist = acc[item._id].matched_uai.includes(item.matched_uai);
-            if (acc[item._id].matched_uai !== item.matched_uai) {
-              if (!exist) {
-                acc[item._id].matched_uai = acc[item._id].matched_uai.concat([item.matched_uai]);
-              }
-            }
+        if (uai_formation) {
+          let resuai = await getEtablissements({ uai: uai_formation });
+
+          if (resuai.length > 0) {
+            resuai.forEach((x) => {
+              const formatted = etablissement(x);
+              etablissements.push({ ...formatted, matched_uai: "UAI_FORMATION" });
+            });
           }
-          return acc;
-        }, {});
+        }
 
-        const formatted = Object.values(result).map((x) => {
-          return {
-            _id: mongoose.Types.ObjectId(),
-            ...x,
-          };
-        });
+        if (etablissement_formateur_uai) {
+          let resformateur = await getEtablissements({ uai: etablissement_formateur_uai });
 
-        logger.info(`${formatted.length} etablissement ajouté - id_formation : ${_id}`);
-        await AfFormation.findByIdAndUpdate(_id, {
-          matching_mna_etablissement: formatted,
-        });
-      })
+          if (resformateur.length > 0) {
+            resformateur.forEach((x) => {
+              const formatted = etablissement(x);
+              etablissements.push({ ...formatted, matched_uai: "UAI_FORMATEUR" });
+            });
+          }
+        }
+
+        if (etablissement_gestionnaire_uai) {
+          let resgestionnaire = await getEtablissements({ uai: etablissement_gestionnaire_uai });
+
+          if (resgestionnaire.length > 0) {
+            resgestionnaire.forEach((x) => {
+              const formatted = etablissement(x);
+              etablissements.push({ ...formatted, matched_uai: "UAI_GESTIONNAIRE" });
+            });
+          }
+        }
+      }
     );
 
-    offset += limit;
-    logger.info(`progress ${computed}/${total}`);
-  }
+    if (etablissements.length === 0) return;
+
+    const result = etablissements.reduce((acc, item) => {
+      if (!acc[item._id]) {
+        acc[item._id] = item;
+        acc[item._id].matched_uai = [acc[item._id].matched_uai];
+      } else {
+        const exist = acc[item._id].matched_uai.includes(item.matched_uai);
+        if (acc[item._id].matched_uai !== item.matched_uai) {
+          if (!exist) {
+            acc[item._id].matched_uai = acc[item._id].matched_uai.concat([item.matched_uai]);
+          }
+        }
+      }
+      return acc;
+    }, {});
+
+    const formatted = Object.values(result).map((x) => {
+      return {
+        _id: mongoose.Types.ObjectId(),
+        ...x,
+      };
+    });
+
+    logger.info(`${formatted.length} etablissement ajouté - id_formation : ${_id}`);
+    await AfFormation.findByIdAndUpdate(_id, {
+      matching_mna_etablissement: formatted,
+    });
+  });
+
   logger.info(`--- END ETABLISSEMENT COVERAGE ---`);
 
   // await oleoduc(
