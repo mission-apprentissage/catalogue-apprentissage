df5548e5179d7e5b53989cc618429ac9affa69e5
Kevin Barnoin <k.barnoin@gmail.com>
2021-01-28 15:27:50 +0100

    fix: new algo

--- server/src/jobs/afFormation/afcoverageEtablissement.js
+++ server/src/jobs/afFormation/afcoverageEtablissement.js
@@ -1,29 +1,34 @@
-const { oleoduc, writeData, transformData } = require("oleoduc");
+// const { oleoduc, writeData, transformData } = require("oleoduc");
 const { AfFormation } = require("../../common/model");
 const logger = require("../../common/logger");
 const { etablissement } = require("./mapper");
-const { Readable } = require("stream");
+// const { Readable } = require("stream");
 const mongoose = require("mongoose");
+const { asyncForEach } = require("../../common/utils/asyncUtils");
 
 module.exports = async (catalogue) => {
   logger.info(`--- START ETABLISSEMENT COVERAGE ---`);
 
   const getEtablissements = (query) => catalogue.getEtablissements({ query });
 
-  await oleoduc(
-    AfFormation.find({ matching_type: { $ne: null } })
-      .lean()
-      .cursor(),
-    transformData((formation) => {
-      return { formations: formation.matching_mna_formation, id: formation._id };
-    }),
-    transformData(
-      async ({ formations, id }) => {
+  let offset = 0;
+  let limit = 5;
+  let computed = 0;
+  let nbFormations = 10;
+
+  while (computed < nbFormations) {
+    let { docs, total } = await AfFormation.paginate({}, { offset, limit });
+    nbFormations = total;
+
+    await Promise.all(
+      docs.map(async ({ matching_mna_formation, _id }) => {
         let etablissements = [];
 
-        await oleoduc(
-          Readable.from(formations),
-          writeData(async ({ uai_formation, etablissement_formateur_uai, etablissement_gestionnaire_uai }) => {
+        computed += 1;
+
+        await asyncForEach(
+          matching_mna_formation,
+          async ({ uai_formation, etablissement_formateur_uai, etablissement_gestionnaire_uai }) => {
             let exist = etablissements.find(
               (x) =>
                 x.uai === uai_formation ||
@@ -65,43 +70,137 @@ module.exports = async (catalogue) => {
                 });
               }
             }
-          })
+          }
         );
 
-        return { etablissements: etablissements, id: id };
-      }
-      // { parallel: 10 }
-    ),
-    writeData(async ({ etablissements, id }) => {
-      if (etablissements.length === 0) return;
+        if (etablissements.length === 0) return;
 
-      const result = etablissements.reduce((acc, item) => {
-        if (!acc[item._id]) {
-          acc[item._id] = item;
-          acc[item._id].matched_uai = [acc[item._id].matched_uai];
-        } else {
-          const exist = acc[item._id].matched_uai.includes(item.matched_uai);
-          if (acc[item._id].matched_uai !== item.matched_uai) {
-            if (!exist) {
-              acc[item._id].matched_uai = acc[item._id].matched_uai.concat([item.matched_uai]);
+        const result = etablissements.reduce((acc, item) => {
+          if (!acc[item._id]) {
+            acc[item._id] = item;
+            acc[item._id].matched_uai = [acc[item._id].matched_uai];
+          } else {
+            const exist = acc[item._id].matched_uai.includes(item.matched_uai);
+            if (acc[item._id].matched_uai !== item.matched_uai) {
+              if (!exist) {
+                acc[item._id].matched_uai = acc[item._id].matched_uai.concat([item.matched_uai]);
+              }
             }
           }
-        }
-        return acc;
-      }, {});
+          return acc;
+        }, {});
 
-      const formatted = Object.values(result).map((x) => {
-        return {
-          _id: mongoose.Types.ObjectId(),
-          ...x,
-        };
-      });
+        const formatted = Object.values(result).map((x) => {
+          return {
+            _id: mongoose.Types.ObjectId(),
+            ...x,
+          };
+        });
 
-      logger.info(`${formatted.length} etablissement ajouté - id_formation : ${id}`);
-      await AfFormation.findByIdAndUpdate(id, {
-        matching_mna_etablissement: formatted,
-      });
-    })
-  );
+        logger.info(`${formatted.length} etablissement ajouté - id_formation : ${_id}`);
+        await AfFormation.findByIdAndUpdate(_id, {
+          matching_mna_etablissement: formatted,
+        });
+      })
+    );
+
+    offset += limit;
+    logger.info(`progress ${computed}/${total}`);
+  }
   logger.info(`--- END ETABLISSEMENT COVERAGE ---`);
+
+  // await oleoduc(
+  //   AfFormation.find({ matching_type: { $ne: null } })
+  //     .lean()
+  //     .cursor(),
+  //   transformData((formation) => {
+  //     return { formations: formation.matching_mna_formation, id: formation._id };
+  //   }),
+  //   transformData(
+  //     async ({ formations, id }) => {
+  //       let etablissements = [];
+
+  //       await oleoduc(
+  //         Readable.from(formations),
+  //         writeData(async ({ uai_formation, etablissement_formateur_uai, etablissement_gestionnaire_uai }) => {
+  //           let exist = etablissements.find(
+  //             (x) =>
+  //               x.uai === uai_formation ||
+  //               x.uai === etablissement_formateur_uai ||
+  //               x.uai === etablissement_gestionnaire_uai
+  //           );
+
+  //           if (exist) return;
+
+  //           if (uai_formation) {
+  //             let resuai = await getEtablissements({ uai: uai_formation });
+
+  //             if (resuai.length > 0) {
+  //               resuai.forEach((x) => {
+  //                 const formatted = etablissement(x);
+  //                 etablissements.push({ ...formatted, matched_uai: "UAI_FORMATION" });
+  //               });
+  //             }
+  //           }
+
+  //           if (etablissement_formateur_uai) {
+  //             let resformateur = await getEtablissements({ uai: etablissement_formateur_uai });
+
+  //             if (resformateur.length > 0) {
+  //               resformateur.forEach((x) => {
+  //                 const formatted = etablissement(x);
+  //                 etablissements.push({ ...formatted, matched_uai: "UAI_FORMATEUR" });
+  //               });
+  //             }
+  //           }
+
+  //           if (etablissement_gestionnaire_uai) {
+  //             let resgestionnaire = await getEtablissements({ uai: etablissement_gestionnaire_uai });
+
+  //             if (resgestionnaire.length > 0) {
+  //               resgestionnaire.forEach((x) => {
+  //                 const formatted = etablissement(x);
+  //                 etablissements.push({ ...formatted, matched_uai: "UAI_GESTIONNAIRE" });
+  //               });
+  //             }
+  //           }
+  //         })
+  //       );
+
+  //       return { etablissements: etablissements, id: id };
+  //     }
+  //     // { parallel: 10 }
+  //   ),
+  //   writeData(async ({ etablissements, id }) => {
+  //     if (etablissements.length === 0) return;
+
+  //     const result = etablissements.reduce((acc, item) => {
+  //       if (!acc[item._id]) {
+  //         acc[item._id] = item;
+  //         acc[item._id].matched_uai = [acc[item._id].matched_uai];
+  //       } else {
+  //         const exist = acc[item._id].matched_uai.includes(item.matched_uai);
+  //         if (acc[item._id].matched_uai !== item.matched_uai) {
+  //           if (!exist) {
+  //             acc[item._id].matched_uai = acc[item._id].matched_uai.concat([item.matched_uai]);
+  //           }
+  //         }
+  //       }
+  //       return acc;
+  //     }, {});
+
+  //     const formatted = Object.values(result).map((x) => {
+  //       return {
+  //         _id: mongoose.Types.ObjectId(),
+  //         ...x,
+  //       };
+  //     });
+
+  //     logger.info(`${formatted.length} etablissement ajouté - id_formation : ${id}`);
+  //     await AfFormation.findByIdAndUpdate(id, {
+  //       matching_mna_etablissement: formatted,
+  //     });
+  //   })
+  // );
+  // logger.info(`--- END ETABLISSEMENT COVERAGE ---`);
 };
