6eeb7cec26142d4fd0a5029be326002eda7102e5
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-02-01 11:20:34 +0100

    chore: rm tabler from dashboard page

--- ui/src/pages/DashboardPage.js
+++ ui/src/pages/DashboardPage.js
@@ -1,30 +1,51 @@
 import React from "react";
-import { Page, Grid, StatsCard, Header } from "tabler-react";
 import { useFetch } from "../common/hooks/useFetch";
-
-import "./DashboardPage.css";
+import {
+  Box,
+  Container,
+  Heading,
+  Grid,
+  GridItem,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatArrow,
+  StatHelpText,
+} from "@chakra-ui/react";
+import Layout from "./layout/Layout";
 
 export default () => {
   const [data, loading] = useFetch("api/stats");
-  console.log(data);
 
   return (
-    <Page>
-      <Page.Main>
-        <Page.Content title="Tableau de bord">
+    <Layout>
+      <Box bg="secondaryBackground" w="100%" py={[1, 8]} px={[1, 24]}>
+        <Container maxW="xl">
+          <Heading as="h1" mb={8} mt={2}>
+            Tableau de bord
+          </Heading>
           {loading && "Chargement des données..."}
           {data && (
             <>
-              <Header.H5>Accueil admin</Header.H5>
-              <Grid.Row cards={true}>
-                <Grid.Col sm={4} lg={2}>
-                  <StatsCard layout={1} movement={0} total={data.stats.nbItems} label="Items" />
-                </Grid.Col>
-              </Grid.Row>
+              <Heading as="h2" fontSize="gamma" mb={4}>
+                Accueil admin
+              </Heading>
+              <Grid templateColumns="repeat(12, 1fr)" gap={2}>
+                <GridItem colSpan={[6, 3]} bg="white" p={4} borderRadius={4}>
+                  <Stat textAlign="center">
+                    <StatLabel>Items</StatLabel>
+                    <StatNumber>{data.stats.nbItems}</StatNumber>
+                    <StatHelpText>
+                      <StatArrow type="increase" />
+                      0%
+                    </StatHelpText>
+                  </Stat>
+                </GridItem>
+              </Grid>
             </>
           )}
-        </Page.Content>
-      </Page.Main>
-    </Page>
+        </Container>
+      </Box>
+    </Layout>
   );
 };
