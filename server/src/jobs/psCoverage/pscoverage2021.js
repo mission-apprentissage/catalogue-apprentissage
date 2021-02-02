ccd4026d5d01ff54598a1e0d3579b1f388ad257a
Kevin Barnoin <k.barnoin@gmail.com>
2021-02-02 15:27:22 +0100

    wip psup 2021

--- server/src/jobs/psCoverage/pscoverage2021.js
+++ server/src/jobs/psCoverage/pscoverage2021.js
@@ -4,8 +4,8 @@ const { PsFormation } = require("../../common/model");
 const { getJsonFromXlsxFile } = require("../../common/utils/fileUtils");
 const { runScript } = require("../scriptWrapper");
 const { Readable } = require("stream");
-
 const { oleoduc, writeData } = require("oleoduc");
+const XLSX = require("xlsx");
 
 const getLibelle = (libelle) => {
   let result = libelle.match(/.*?(?=\s- en apprentissage)/);
@@ -13,7 +13,6 @@ const getLibelle = (libelle) => {
   if (!result) return libelle;
 
   if (result[0].includes("(Bac") || result[0].includes("(BAC")) {
-    console.log("result[0]----", result[0]);
     result = result[0].match(/.*?(?=\s\(bac \+)/i)[0];
     return result;
   }
@@ -25,100 +24,119 @@ const run = async (tableCorrespondance) => {
   const filePath = path.resolve(__dirname, "./assets/formation-psup-2021.xls");
   const f2021 = getJsonFromXlsxFile(filePath);
 
+  let notFound = [];
+
   const f2020 = await PsFormation.find({});
 
   const stats = {
     total2020: f2020.length,
     total2021: f2021.length,
-    existing: 0,
+    existingWithMef: 0,
     new: 0,
-    existUAI: 0,
+    existingWithCfd: 0,
+    notFound: 0,
+    existingUAIOnly: 0,
+    existingUAICP: 0,
   };
 
   await oleoduc(
     Readable.from(f2021),
     writeData(async (formation) => {
-      let { UAI_GES, UAI_COMPOSANTE, UAI_AFF, CODEMEF, LIBSPÉCIALITÉ } = formation;
+      let { UAI_GES, UAI_COMPOSANTE, UAI_AFF, CODEMEF, LIBSPÉCIALITÉ, CODEPOSTAL } = formation;
 
       if (CODEMEF) {
-        let found = await PsFormation.find({
+        let matchMEF = await PsFormation.find({
           uai_gestionnaire: UAI_GES,
           uai_composante: UAI_COMPOSANTE,
           uai_affilie: UAI_AFF,
           code_mef_10: CODEMEF,
         });
 
-        if (found) {
-          stats.existing += 1;
-        } else {
+        if (!matchMEF) {
           stats.new += 1;
         }
-      } else {
-        if (LIBSPÉCIALITÉ) {
-          let LIBELLE_STAT_33 = getLibelle(LIBSPÉCIALITÉ).toUpperCase();
 
-          console.log({
-            LIBSPÉCIALITÉ,
-            LIBELLE_STAT_33,
-          });
-
-          let code_cfd;
-
-          const {
-            formationsDiplomes,
-            pagination: { total },
-          } = await tableCorrespondance.getBcnInfo({
-            query: { LIBELLE_STAT_33 },
-          });
-
-          console.log(total);
-
-          if (total === 0) return null;
-
-          if (total > 1) {
-            const openFormation = formationsDiplomes.filter((x) => x.DATE_FERMETURE === "");
-            if (openFormation.length === 0) return null;
-            return openFormation[0].FORMATION_DIPLOME;
-          }
-
-          code_cfd = formationsDiplomes[0].FORMATION_DIPLOME;
-
-          let found = await PsFormation.find({
-            uai_gestionnaire: UAI_GES,
-            uai_composante: UAI_COMPOSANTE,
-            uai_affilie: UAI_AFF,
-            code_cfd,
-          });
-
-          console.log("found", found);
-
-          if (found) {
-            stats.existing += 1;
-          } else {
-            stats.new += 1;
-          }
-        } else {
-          let found = await PsFormation.find({
-            uai_gestionnaire: UAI_GES,
-            uai_composante: UAI_COMPOSANTE,
-            uai_affilie: UAI_AFF,
-          });
-
-          if (found) {
-            stats.existing += 1;
-          } else {
-            stats.new += 1;
-          }
-        }
+        stats.existingWithMef += 1;
+        return;
       }
+
+      let matchCP = await PsFormation.find({
+        uai_gestionnaire: UAI_GES,
+        uai_composante: UAI_COMPOSANTE,
+        uai_affilie: UAI_AFF,
+        code_postal: CODEPOSTAL,
+      });
+
+      // console.log("matchCP", matchCP);
+
+      if (matchCP.length > 1) {
+        stats.existingUAICP += 1;
+        return;
+      }
+
+      let LIBELLE_STAT_33 = getLibelle(LIBSPÉCIALITÉ).toUpperCase();
+
+      // console.log({
+      //   LIBSPÉCIALITÉ,
+      //   LIBELLE_STAT_33,
+      // });
+
+      let code_cfd;
+
+      const {
+        formationsDiplomes,
+        pagination: { total },
+      } = await tableCorrespondance.getBcnInfo({
+        query: { LIBELLE_STAT_33 },
+      });
+
+      // console.log(total);
+
+      if (total === 0) {
+        console.log("formation", formation);
+        notFound.push(formation);
+        stats.notFound += 1;
+        return;
+      }
+
+      if (total === 1) code_cfd = formationsDiplomes[0].FORMATION_DIPLOME;
+
+      if (total > 1) {
+        const openFormation = formationsDiplomes.filter((x) => x.DATE_FERMETURE === "");
+        if (openFormation.length === 0) return null;
+        code_cfd = openFormation[0].FORMATION_DIPLOME;
+      }
+
+      let matchCFD = await PsFormation.find({
+        uai_gestionnaire: UAI_GES,
+        uai_composante: UAI_COMPOSANTE,
+        uai_affilie: UAI_AFF,
+        code_cfd,
+      });
+
+      if (matchCFD) {
+        stats.existingWithCfd += 1;
+        return;
+      }
+
+      console.log("formation-end", formation);
+      notFound.push(formation);
+      stats.notFound += 1;
     })
   );
 
   console.log("stats", stats);
+  console.log("notFound.length", notFound.length);
 
-  // await asyncForEach(f2021, async ({ UAI_GES, UAI_COMPOSANTE, UAI_AFF, CODEMEF }) => {
-  //   console.log("formation", formation);
-  // });
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(notFound), "psup");
+
+  XLSX.writeFileAsync(path.join(__dirname, `/assets/export-psup2021-missing-mef-cfd.xlsx`), workbook, (e) => {
+    if (e) {
+      console.log(e);
+      throw new Error("La génération du fichier excel à échoué : ", e);
+    }
+  });
 };
 
 if (process.env.standalone) {
