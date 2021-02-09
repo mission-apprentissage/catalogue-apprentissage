02b364d4d485b771c979fecde0861e8372f42daa
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-02-09 17:16:35 +0100

    fix: rebuild index convertedformations

--- server/src/jobs/esIndex/esIndex.js
+++ server/src/jobs/esIndex/esIndex.js
@@ -1,24 +1,14 @@
 const { rebuildIndex } = require("../../common/utils/esUtils");
-const { MnaFormation, RcoFormation, ConvertedFormation } = require("../../common/model/index");
+const { ConvertedFormation } = require("../../common/model/index");
 
 const rebuildEsIndex = async (index, skipNotFound = false) => {
   switch (index) {
-    case "mnaformation":
-      await rebuildIndex("mnaformation", MnaFormation, { skipNotFound });
-      break;
-
-    case "rcoformation":
-      await rebuildIndex("rcoformation", RcoFormation, { skipNotFound });
-      break;
-
-    case "convertedformation":
-      await rebuildIndex("convertedformation", ConvertedFormation, { skipNotFound });
+    case "convertedformations":
+      await rebuildIndex("convertedformations", ConvertedFormation, { skipNotFound });
       break;
 
     default:
-      await rebuildIndex("mnaformation", MnaFormation);
-      await rebuildIndex("rcoformation", RcoFormation);
-      await rebuildIndex("convertedformation", ConvertedFormation);
+      await rebuildIndex("convertedformations", ConvertedFormation);
   }
 };
 
