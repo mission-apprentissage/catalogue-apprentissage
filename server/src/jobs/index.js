7a9637a6bb99c7240d07aea267a38bc3a68b6548
Kevin Barnoin <k.barnoin@gmail.com>
2021-02-09 12:52:13 +0100

    fix: update night job

--- server/src/jobs/index.js
+++ server/src/jobs/index.js
@@ -5,8 +5,8 @@ const rcoConverter = require("./rcoConverter");
 const trainingsUpdater = require("./trainingsUpdater");
 const psReference = require("./psReference");
 const afReference = require("./afReference");
-// const afCoverage = require("./afFormation/index");
-// const afReconciliation = require("./afFormation/reconciliation");
+const afCoverage = require("./afFormation/index");
+const afReconciliation = require("./afFormation/reconciliation");
 
 const clean = require("./clean");
 const { importEtablissements } = require("./etablissements");
@@ -27,8 +27,8 @@ runScript(async ({ catalogue }) => {
     await psReference();
 
     // affelnet
-    // await afCoverage(); // TODO @kevbarns uncomment
-    // await afReconciliation(); // TODO @kevbarns uncomment
+    await afCoverage();
+    await afReconciliation();
     await afReference();
   } catch (error) {
     logger.error(error);
