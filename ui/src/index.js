0e4ccc1d8b32b07aa0976e7d23ad74c08322c102
Kevin Barnoin <k.barnoin@gmail.com>
2021-02-15 15:28:10 +0100

    fix: remove strict mode

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,28 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/index";
 
-// if (
-//   window.location.hostname.includes("catalogue.apprentissage.beta.gouv.fr") &&
-//   window.location.pathname !== "/report" &&
-//   window.location.pathname !== "/coverage"
-// ) {
-//   window.location.replace("https://mna-admin-prod.netlify.app");
-// }
-
 ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme} resetCSS>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+  <ChakraProvider theme={theme} resetCSS>
+    <App />
+  </ChakraProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
