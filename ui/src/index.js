1e213640d1906a15d609bd90aee1d8da250cfee8
Kevin Barnoin <k.barnoin@gmail.com>
2021-02-15 15:55:59 +0100

    fix: strict mode on

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,8 +6,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/index";
 
 ReactDOM.render(
-  <ChakraProvider theme={theme} resetCSS>
-    <App />
-  </ChakraProvider>,
+  <React.StrictMode>
+    <ChakraProvider theme={theme} resetCSS>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
