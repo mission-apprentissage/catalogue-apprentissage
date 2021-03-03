2e4cbe395b226958ef13d3c7e57e054e4e8c9b71
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-03 18:48:25 +0100

    fix: serge script !

--- server/src/jobs/parcoursup/miscJobs/get-rncp-romes-cfd.js
+++ server/src/jobs/parcoursup/miscJobs/get-rncp-romes-cfd.js
@@ -2,19 +2,42 @@ const path = require("path");
 const XLSX = require("xlsx");
 const { uniqBy } = require("lodash");
 const { runScript } = require("../../scriptWrapper");
+const stringSimilarity = require("string-similarity");
 const { paginator } = require("../../common/utils/paginator");
 const { asyncForEach } = require("../../../common/utils/asyncUtils");
 const { getJsonFromXlsxFile } = require("../../../common/utils/fileUtils");
 const { AfReconciliation, ConvertedFormation } = require("../../../common/model");
-const { getCfdInfo, getMefInfo } = require("../../../common/services/tables_correspondance");
+const { getCfdInfo, getMefInfo, getBcnInfo } = require("../../../common/services/tables_correspondance");
 
 runScript(async () => {
   await psup2021();
 });
 
+const getLibelleLong = (libelle) =>
+  libelle
+    .split(" - en apprentissage")[0]
+    .replace(/\((.)*\)+/g, "")
+    .trim()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toUpperCase();
+
+const getLibelleCourt = (libelle) => {
+  if (libelle.includes("BUT")) return "DUT";
+  if (libelle.includes("Formation professionnelle")) return "";
+  if (libelle.includes("Formations  des écoles d'ingénieurs")) return "";
+  if (libelle.includes("Certificat de Spécialisation Agricole")) return "";
+  if (libelle.includes("BPJEPS")) return "";
+  if (libelle.includes("Sous-officier")) return "";
+};
+
 async function psup2021() {
   const file = path.resolve(__dirname, "../assets/formation-psup-2021_26022021.xls");
   const data = getJsonFromXlsxFile(file);
+  const bcnDb = await getBcnInfo({ query: {}, limit: 10000 });
+
+  const bcnClean = bcnDb.formationsDiplomes.filter((x) => x.LIBELLE_LONG_200 !== "" && x.LIBELLE_LONG_200 !== null);
+  const labels = bcnClean.map((x) => x.LIBELLE_LONG_200);
 
   const filtered = uniqBy(data, "CODESPÉCIALITÉ");
 
@@ -25,23 +48,22 @@ async function psup2021() {
     if (item.CODEMEF) {
       try {
         let responseMEF = await getMefInfo(item.CODEMEF);
-        // console.log("responseMEF", responseMEF);
+
         if (responseMEF) {
           let romes = responseMEF.result.rncp?.romes?.map((x) => x.rome);
-          item.CODE_RNCP = responseMEF.result.rncp?.code_rncp || "Non trouvé";
+          item.CODE_RNCP = responseMEF.result.rncp?.code_rncp ?? "Non trouvé";
           item.CODE_ROME = romes ? romes.join() : "Non trouvé";
-          item.CODE_CFD_MNA = responseMEF.result.cfd?.cfd || "Non trouvé";
+          item.CODE_CFD_MNA = responseMEF.result.cfd?.cfd ?? "Non trouvé";
 
           if (
             responseMEF.messages.rncp.error === "Erreur: Non trouvé" &&
             responseMEF.messages.cfdUpdated === "Trouvé"
           ) {
-            console.log("coucou");
             let responseCFD = await getCfdInfo(item.CODE_CFD_MNA);
 
             if (responseCFD) {
               let romes = responseCFD.result.rncp?.romes?.map((x) => x.rome);
-              item.CODE_RNCP = responseCFD.result.rncp?.code_rncp || "Non trouvé";
+              item.CODE_RNCP = responseCFD.result.rncp?.code_rncp ?? "Non trouvé";
               item.CODE_ROME = romes ? romes.join() : "Non trouvé";
             }
           }
@@ -58,16 +80,13 @@ async function psup2021() {
       console.log(item.CODEDIPLOME_MAP);
       try {
         let responseCFD = await getCfdInfo(item.CODEDIPLOME_MAP);
-        // console.log("responseCFD", responseCFD);
 
         if (responseCFD) {
-          // if (responseCFD.messages.cfd?.includes("Trouvé")) {
           let romes = responseCFD.result.rncp?.romes?.map((x) => x.rome);
 
-          item.CODE_CFD_MNA = responseCFD.result.cfd || "Non trouvé";
-          item.CODE_RNCP = responseCFD.result.rncp?.code_rncp || "Non trouvé";
+          item.CODE_CFD_MNA = responseCFD.result.cfd ?? "Non trouvé";
+          item.CODE_RNCP = responseCFD.result.rncp?.code_rncp ?? "Non trouvé";
           item.CODE_ROME = romes ? romes.join() : "Non trouvé";
-          // }
 
           updated.push(item);
         }
@@ -75,6 +94,68 @@ async function psup2021() {
         console.log("getCfdInfo", error);
       }
       return;
+    }
+
+    if (!item.CODEMEF && !item.CODE_CFD_MNA) {
+      let LIBELLE_LONG_200 = getLibelleLong(item.LIBSPÉCIALITÉ);
+      let LIBELLE_COURT = getLibelleCourt(item.LIBFORMATION);
+
+      try {
+        const {
+          formationsDiplomes,
+          pagination: { total },
+        } = await getBcnInfo({ query: { LIBELLE_LONG_200 } });
+
+        if (total > 0) {
+          const openFormation = formationsDiplomes.filter((x) => x.LIBELLE_COURT === LIBELLE_COURT);
+
+          if (openFormation.length === 0) {
+            let match = [];
+
+            let { bestMatch, bestMatchIndex } = stringSimilarity.findBestMatch(LIBELLE_LONG_200, labels);
+
+            if (bestMatch.rating > 0.75) {
+              match.push({
+                bestMatch,
+                data: bcnClean[bestMatchIndex],
+              });
+            }
+
+            let found = match.filter((x) => x.data.LIBELLE_COURT === LIBELLE_COURT);
+
+            if (found.length > 0) {
+              console.log("found", found);
+              item.CODE_CFD_MNA = found[0].FORMATION_DIPLOME;
+            }
+
+            if (found.length === 0 && match.length > 0) {
+              console.log("match but not found", match);
+              item.CODE_CFD_MNA = match[0].FORMATION_DIPLOME;
+            }
+          } else {
+            item.CODE_CFD_MNA = openFormation[0].FORMATION_DIPLOME;
+          }
+        }
+      } catch (error) {
+        console.log("getBcnInfo", error);
+      }
+
+      if (item.CODE_CFD_MNA) {
+        try {
+          let responseCFD = await getCfdInfo(item.CODE_CFD_MNA);
+
+          if (responseCFD) {
+            let romes = responseCFD.result.rncp?.romes?.map((x) => x.rome);
+            item.CODE_RNCP = responseCFD.result.rncp?.code_rncp ?? "Non trouvé";
+            item.CODE_ROME = romes ? romes.join() : "Non trouvé";
+          }
+          updated.push(item);
+
+          return;
+        } catch (error) {
+          console.log("getBcnInfo", error);
+        }
+      }
     }
 
     item.CODE_RNCP = "Non trouvé";
