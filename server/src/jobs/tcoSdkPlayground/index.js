a40a1d7731a670a970952b9c34b2cb9010fe67df
Gethi <bigard.antoine@gmail.com>
2021-03-04 17:56:28 +0100

    chore: wip

--- server/src/jobs/tcoSdkPlayground/index.js
+++ server/src/jobs/tcoSdkPlayground/index.js
@@ -1,8 +1,15 @@
 const { runScript } = require("../scriptWrapper");
 const { mongoose } = require("../../common/mongodb");
-const { initTcoModel, getCpInfo } = require("@mission-apprentissage/tco-service-node");
+const {
+  initTcoModel,
+  //getCpInfo,
+  getRncpInfo,
+  rncpImporter,
+} = require("@mission-apprentissage/tco-service-node");
 
 runScript(async () => {
   await initTcoModel(mongoose);
-  console.log(await getCpInfo("92600"));
+  // console.log(await getCpInfo("92600"));
+  await rncpImporter();
+  console.log(await getRncpInfo("RNCP7571"));
 });
