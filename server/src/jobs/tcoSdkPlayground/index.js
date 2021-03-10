333238dc81c1c7a8459501f7d0fe551d68fb479e
Gethi <bigard.antoine@gmail.com>
2021-03-10 14:13:14 +0100

    feat: add cfd info

--- server/src/jobs/tcoSdkPlayground/index.js
+++ server/src/jobs/tcoSdkPlayground/index.js
@@ -2,14 +2,20 @@ const { runScript } = require("../scriptWrapper");
 const { mongoose } = require("../../common/mongodb");
 const {
   initTcoModel,
-  //getCpInfo,
-  getRncpInfo,
-  rncpImporter,
+  // getCpInfo,
+  // rncpImporter,
+  // getRncpInfo,
+  bcnImporter,
+  getCfdInfo,
 } = require("@mission-apprentissage/tco-service-node");
 
 runScript(async () => {
   await initTcoModel(mongoose);
   // console.log(await getCpInfo("92600"));
-  await rncpImporter();
-  console.log(await getRncpInfo("RNCP7571"));
+
+  // await rncpImporter();
+  // console.log(await getRncpInfo("RNCP7571"));
+
+  await bcnImporter();
+  console.log(await getCfdInfo("26033206"));
 });
