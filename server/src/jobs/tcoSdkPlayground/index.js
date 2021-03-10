b92ab973ba66a3b1c563dab3e1364205aca158b1
Gethi <bigard.antoine@gmail.com>
2021-03-10 17:02:49 +0100

    chore: up

--- server/src/jobs/tcoSdkPlayground/index.js
+++ server/src/jobs/tcoSdkPlayground/index.js
@@ -5,8 +5,10 @@ const {
   // getCpInfo,
   // rncpImporter,
   // getRncpInfo,
-  bcnImporter,
-  getCfdInfo,
+  // bcnImporter,
+  // getCfdInfo,
+  // getMef10Info,
+  getSiretInfo,
 } = require("@mission-apprentissage/tco-service-node");
 
 runScript(async () => {
@@ -16,6 +18,9 @@ runScript(async () => {
   // await rncpImporter();
   // console.log(await getRncpInfo("RNCP7571"));
 
-  await bcnImporter();
-  console.log(await getCfdInfo("26033206"));
+  //await bcnImporter();
+  //console.log(await getCfdInfo("26033206"));
+  // console.log(await getMef10Info("23310022319"));
+
+  console.log(await getSiretInfo("32922456200234"));
 });
