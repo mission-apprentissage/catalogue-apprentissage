5ce878bbca13014c16f1e5ee0eb8fdf3ea6100da
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-16 18:00:52 +0100

    feat: add title to stats

--- server/src/common/components/stats.js
+++ server/src/common/components/stats.js
@@ -3,73 +3,128 @@ const { AfFormation, AfReconciliation, RcoFormation, ConvertedFormation, PsForma
 async function getAllStats() {
   // Catalogue
   const nbCataloguePublished = {
-    title: "Nombre de formation publié",
+    title: "Formations publiées",
     value: await ConvertedFormation.countDocuments({ published: true }),
   };
 
   // RCO
-  const nbRcoPublished = await RcoFormation.countDocuments({ published: true });
-  const nbRcoConversionError = await RcoFormation.countDocuments({ converted_to_mna: { $ne: true } });
+  const nbRcoPublished = {
+    title: "Formations RCO publiées",
+    value: await RcoFormation.countDocuments({ published: true }),
+  };
+  const nbRcoConversionError = {
+    title: "Formations RCO en erreur",
+    value: await RcoFormation.countDocuments({ converted_to_mna: { $ne: true } }),
+  };
 
   // Parcoursup
-  const nbAllPsup = await PsFormation2021.countDocuments();
-  const nbAllPsupReconcilied = await PsFormation2021.countDocuments({ etat_reconciliation: true });
-  const nbPsupErrors = await ConvertedFormation.countDocuments({ published: true, parcoursup_error: { $ne: null } });
-  const nbPsupNotRelevant = await ConvertedFormation.countDocuments({
-    published: true,
-    parcoursup_statut: "hors périmètre",
-  });
-  const nbPsupToCheck = await ConvertedFormation.countDocuments({ published: true, parcoursup_statut: "à publier" });
-  const nbPsupPending = await ConvertedFormation.countDocuments({
-    published: true,
-    parcoursup_statut: "en attente de publication",
-  });
-  const nbPsupPublished = await ConvertedFormation.countDocuments({ published: true, parcoursup_statut: "publié" });
-  const nbPsupNotPublished = await ConvertedFormation.countDocuments({
-    published: true,
-    parcoursup_statut: "non publié",
-  });
+  const nbAllPsup = {
+    title: "Nombre de formations",
+    value: await PsFormation2021.countDocuments(),
+  };
+  const nbAllPsupReconcilied = {
+    title: "Formation réconcilié",
+    value: await PsFormation2021.countDocuments({ etat_reconciliation: true }),
+  };
+  const nbPsupErrors = {
+    title: "Formations en erreur",
+    value: await ConvertedFormation.countDocuments({ published: true, parcoursup_error: { $ne: null } }),
+  };
+  const nbPsupNotRelevant = {
+    title: "Formations hors périmètre",
+    value: await ConvertedFormation.countDocuments({
+      published: true,
+      parcoursup_statut: "hors périmètre",
+    }),
+  };
+  const nbPsupToCheck = {
+    title: "Formations à publier",
+    value: await ConvertedFormation.countDocuments({ published: true, parcoursup_statut: "à publier" }),
+  };
+  const nbPsupPending = {
+    title: "Formations en attente de publication",
+    value: await ConvertedFormation.countDocuments({
+      published: true,
+      parcoursup_statut: "en attente de publication",
+    }),
+  };
+  const nbPsupPublished = {
+    title: "Formations publiées",
+    value: await ConvertedFormation.countDocuments({ published: true, parcoursup_statut: "publié" }),
+  };
+  const nbPsupNotPublished = {
+    title: "Formations non publiées",
+    value: await ConvertedFormation.countDocuments({
+      published: true,
+      parcoursup_statut: "non publié",
+    }),
+  };
 
   // Affelnet
-  const nbAllAffelnet = await AfFormation.countDocuments();
-  const nbAllAffelnetReconcilied = await AfReconciliation.countDocuments();
-  const nbAffelnetErrors = await ConvertedFormation.countDocuments({ published: true, affelnet_error: { $ne: null } });
-  const nbAffelnetNotRelevant = await ConvertedFormation.countDocuments({
-    published: true,
-    affelnet_statut: "hors périmètre",
-  });
-  const nbAffelnetToCheck = await ConvertedFormation.countDocuments({ published: true, affelnet_statut: "à publier" });
-  const nbAffelnetPending = await ConvertedFormation.countDocuments({
-    published: true,
-    affelnet_statut: "en attente de publication",
-  });
-  const nbAffelnetPublished = await ConvertedFormation.countDocuments({ published: true, affelnet_statut: "publié" });
-  const nbAffelnetNotPublished = await ConvertedFormation.countDocuments({
-    published: true,
-    affelnet_statut: "non publié",
-  });
+  const nbAllAffelnet = {
+    title: "Nombre de formations",
+    value: await AfFormation.countDocuments(),
+  };
+  const nbAllAffelnetReconcilied = {
+    title: "Formations réconciliées",
+    value: await AfReconciliation.countDocuments(),
+  };
+  const nbAffelnetErrors = {
+    title: "Formations en erreur",
+    value: await ConvertedFormation.countDocuments({ published: true, affelnet_error: { $ne: null } }),
+  };
+  const nbAffelnetNotRelevant = {
+    title: "Formations hors périmètre",
+    value: await ConvertedFormation.countDocuments({
+      published: true,
+      affelnet_statut: "hors périmètre",
+    }),
+  };
+  const nbAffelnetToCheck = {
+    title: "Formations à publier",
+    value: await ConvertedFormation.countDocuments({ published: true, affelnet_statut: "à publier" }),
+  };
+  const nbAffelnetPending = {
+    title: "Formations en attente de publication",
+    value: await ConvertedFormation.countDocuments({
+      published: true,
+      affelnet_statut: "en attente de publication",
+    }),
+  };
+  const nbAffelnetPublished = {
+    title: "Formations publié",
+    value: await ConvertedFormation.countDocuments({ published: true, affelnet_statut: "publié" }),
+  };
+  const nbAffelnetNotPublished = {
+    title: "Formations non publié",
+    value: await ConvertedFormation.countDocuments({
+      published: true,
+      affelnet_statut: "non publié",
+    }),
+  };
 
   return {
-    catalogue: [],
-    nbCataloguePublished,
-    nbRcoPublished,
-    nbRcoConversionError,
-    nbAllPsup,
-    nbAllPsupReconcilied,
-    nbPsupErrors,
-    nbPsupNotRelevant,
-    nbPsupToCheck,
-    nbPsupPending,
-    nbPsupPublished,
-    nbPsupNotPublished,
-    nbAllAffelnet,
-    nbAllAffelnetReconcilied,
-    nbAffelnetErrors,
-    nbAffelnetNotRelevant,
-    nbAffelnetToCheck,
-    nbAffelnetPending,
-    nbAffelnetPublished,
-    nbAffelnetNotPublished,
+    catalogue: [nbCataloguePublished, nbRcoPublished, nbRcoConversionError],
+    parcoursup: [
+      nbAllPsup,
+      nbAllPsupReconcilied,
+      nbPsupErrors,
+      nbPsupNotRelevant,
+      nbPsupToCheck,
+      nbPsupPending,
+      nbPsupPublished,
+      nbPsupNotPublished,
+    ],
+    affelnet: [
+      nbAllAffelnet,
+      nbAllAffelnetReconcilied,
+      nbAffelnetErrors,
+      nbAffelnetNotRelevant,
+      nbAffelnetToCheck,
+      nbAffelnetPending,
+      nbAffelnetPublished,
+      nbAffelnetNotPublished,
+    ],
   };
 }
 
