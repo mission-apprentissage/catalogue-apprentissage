78043bb4cfb2cb02d475ea8a7024e4dd11a6b9a3
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-16 18:01:45 +0100

    feat: dashboard v0

--- ui/src/pages/Dashboard/index.js
+++ ui/src/pages/Dashboard/index.js
@@ -1,24 +1,11 @@
 import React from "react";
 import { useFetch } from "../../common/hooks/useFetch";
-import {
-  Box,
-  Container,
-  Heading,
-  Grid,
-  GridItem,
-  Stat,
-  StatLabel,
-  StatNumber,
-  StatArrow,
-  StatHelpText,
-} from "@chakra-ui/react";
+import { Box, Container, Heading, Grid, GridItem } from "@chakra-ui/react";
 import Layout from "../layout/Layout";
 import StatCard from "./components/StatCard";
 
 export default () => {
-  // const [data, loading] = useFetch("api/stats");
-  let data = true;
-  let loading = false;
+  const [data, loading] = useFetch("api/v1/stats");
 
   return (
     <Layout>
@@ -33,42 +20,38 @@ export default () => {
               <Heading as="h2" fontSize="gamma" mb={4}>
                 Catalogue
               </Heading>
-              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={2}>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation publiées" stat={data.nbCataloguePublished} />
-                </GridItem>
-                <GridItem>
-                  <StatCard label="Nombre de formation RCO publiées" stat={data.nbRcoPublished} />
-                </GridItem>
-                <GridItem>
-                  <StatCard label="Nombre de formation RCO en erreur" stat={data.nbRcoConversionError} />
-                </GridItem>
+              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
+                {data.catalogue.map((item, i) => {
+                  return (
+                    <GridItem key={i} colSpan={[6, 3]}>
+                      <StatCard label={item.title} stat={item.value} />
+                    </GridItem>
+                  );
+                })}
               </Grid>
               <Heading as="h2" fontSize="gamma" mb={4}>
                 Parcoursup
               </Heading>
-              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={2}>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation" stat={data.nbAllPsup} />
-                </GridItem>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation réconcilées" stat={data.nbAllPsupReconcilied} />
-                </GridItem>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation réconcilées" stat={data.nbPsupErrors} />
-                </GridItem>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation réconcilées" stat={data.nbPsupNotRelevant} />
-                </GridItem>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation réconcilées" stat={data.nbPsupToCheck} />
-                </GridItem>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation réconcilées" stat={data.nbPsupPending} />
-                </GridItem>
-                <GridItem colSpan={[6, 3]}>
-                  <StatCard label="Nombre de formation réconcilées" stat={data.nbPsupPublished} />
-                </GridItem>
+              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
+                {data.parcoursup.map((item, i) => {
+                  return (
+                    <GridItem key={i} colSpan={[6, 3]}>
+                      <StatCard label={item.title} stat={item.value} />
+                    </GridItem>
+                  );
+                })}
+              </Grid>
+              <Heading as="h2" fontSize="gamma" mb={4}>
+                Affelnet
+              </Heading>
+              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
+                {data.affelnet.map((item, i) => {
+                  return (
+                    <GridItem key={i} colSpan={[6, 3]}>
+                      <StatCard label={item.title} stat={item.value} />
+                    </GridItem>
+                  );
+                })}
               </Grid>
             </>
           )}
