b2ebe752347c6da2613a90ad2c57d0e3706a043e
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-03-18 16:03:31 +0100

    perf(reference): faster reference jobs
    :zap: ~ 30s for Ps, ~ 5 min for Af

--- server/src/jobs/reference/affelnet/controller.js
+++ server/src/jobs/reference/affelnet/controller.js
@@ -1,50 +1,34 @@
-const { ConvertedFormation } = require("../../../common/model");
+const { ConvertedFormation, AfReconciliation } = require("../../../common/model");
 const logger = require("../../../common/logger");
-const afReferenceMapper = require("../../../logic/mappers/afReferenceMapper");
 const { paginator } = require("../../common/utils/paginator");
 
 const run = async () => {
+  await ConvertedFormation.updateMany({}, { $set: { last_update_at: Date.now(), affelnet_reference: false } });
+
   //  check for published trainings in affelnet (set "publié") / but don't overwrite those on "non publié" status : it means a user chose not to publish
-  await paginator(
-    ConvertedFormation,
-    {
-      filter: {
+  await paginator(AfReconciliation, { filter: { unpublished_by_user: null } }, async (reconciliation) => {
+    await ConvertedFormation.updateMany(
+      {
         published: true,
         etablissement_reference_catalogue_published: true,
         affelnet_statut: { $ne: "non publié" },
         cfd_outdated: { $ne: true },
+        $or: [
+          {
+            cfd: reconciliation.code_cfd,
+            etablissement_formateur_siret: reconciliation.siret_formateur,
+            etablissement_gestionnaire_siret: reconciliation.siret_gestionnaire,
+          },
+          {
+            cfd: reconciliation.code_cfd,
+            etablissement_formateur_siret: reconciliation.siret_gestionnaire,
+            etablissement_gestionnaire_siret: reconciliation.siret_formateur,
+          },
+        ],
       },
-    },
-    async (formation) => {
-      const { affelnet_reference, messages, error } = await afReferenceMapper({
-        cfd: formation.cfd,
-        siret_formateur: formation.etablissement_formateur_siret,
-        siret_gestionnaire: formation.etablissement_gestionnaire_siret,
-      });
-
-      if (error) {
-        // do nothing error is already logged in mapper
-        return;
-      }
-
-      formation.affelnet_reference = affelnet_reference;
-
-      if (affelnet_reference) {
-        formation.affelnet_error = "success";
-      } else {
-        formation.affelnet_error = messages?.error ?? null;
-      }
-
-      formation.last_update_at = Date.now();
-      await formation.save();
-    }
-  );
-
-  // update affelnet_statut outside loop to not mess up with paginate
-  await ConvertedFormation.updateMany(
-    { affelnet_error: "success" },
-    { $set: { affelnet_error: null, affelnet_statut: "publié" } }
-  );
+      { $set: { last_update_at: Date.now(), affelnet_reference: true, affelnet_statut: "publié" } }
+    );
+  });
 
   // 4 - stats
   const totalPublished = await ConvertedFormation.countDocuments({ published: true });
