7a17d882ec7c8f3441adbd075390f539810c2b14
Kevin Barnoin <k.barnoin@gmail.com>
2021-03-19 21:10:53 +0100

    rafacto: dashboard

--- ui/src/pages/Dashboard/index.js
+++ ui/src/pages/Dashboard/index.js
@@ -4,6 +4,20 @@ import { Box, Container, Heading, Grid, GridItem } from "@chakra-ui/react";
 import Layout from "../layout/Layout";
 import StatCard from "./components/StatCard";
 
+const Component = (props) => {
+  return (
+    <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
+      {props.data?.map((item, i) => {
+        return (
+          <GridItem key={i} colSpan={[6, 3]}>
+            <StatCard label={item.title} stat={item.value} />
+          </GridItem>
+        );
+      })}
+    </Grid>
+  );
+};
+
 export default () => {
   const [data, loading] = useFetch("api/stats");
 
@@ -20,39 +34,15 @@ export default () => {
               <Heading as="h2" fontSize="gamma" mb={4}>
                 Catalogue
               </Heading>
-              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
-                {data.catalogue.map((item, i) => {
-                  return (
-                    <GridItem key={i} colSpan={[6, 3]}>
-                      <StatCard label={item.title} stat={item.value} />
-                    </GridItem>
-                  );
-                })}
-              </Grid>
+              <Component data={data.catalogue} />
               <Heading as="h2" fontSize="gamma" mb={4}>
                 Parcoursup
               </Heading>
-              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
-                {data.parcoursup.map((item, i) => {
-                  return (
-                    <GridItem key={i} colSpan={[6, 3]}>
-                      <StatCard label={item.title} stat={item.value} />
-                    </GridItem>
-                  );
-                })}
-              </Grid>
+              <Component data={data.parcoursup} />
               <Heading as="h2" fontSize="gamma" mb={4}>
                 Affelnet
               </Heading>
-              <Grid templateColumns="repeat(12, 1fr)" gap={2} pb={4}>
-                {data.affelnet.map((item, i) => {
-                  return (
-                    <GridItem key={i} colSpan={[6, 3]}>
-                      <StatCard label={item.title} stat={item.value} />
-                    </GridItem>
-                  );
-                })}
-              </Grid>
+              <Component data={data.affelnet} />
             </>
           )}
         </Container>
