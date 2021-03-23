bdcc592f44c0c55ae64638e78853000ee689e3cb
Gethi <bigard.antoine@gmail.com>
2021-03-23 16:00:49 +0100

    fix: missing slash

--- ui/src/common/components/StatGrid.jsx
+++ ui/src/common/components/StatGrid.jsx
@@ -1,6 +1,6 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import React from "react";
-import StatCard from ".StatCard";
+import StatCard from "./StatCard";
 
 const StatGrid = (props) => {
   return (
