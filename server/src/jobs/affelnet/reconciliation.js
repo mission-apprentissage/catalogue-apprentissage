3796cb6f0332a9990c70e44bea1d3c11fa40de84
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-04-19 18:12:15 +0200

    feat(reconciliation): keep only 1 - 1 match

--- server/src/jobs/affelnet/reconciliation.js
+++ server/src/jobs/affelnet/reconciliation.js
@@ -10,9 +10,43 @@ const afReconciliation = async () => {
 
     await AfReconciliation.deleteMany({ source: { $in: [null, "AUTOMATIQUE"] } });
 
+    const duplicates = await AfFormation.aggregate([
+      { $match: { matching_mna_formation: { $size: 1 } } },
+      {
+        $unwind: "$matching_mna_formation",
+      },
+      {
+        $group: {
+          _id: "$_id",
+          mna: {
+            $first: "$matching_mna_formation",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$mna._id",
+          count: { $sum: 1 },
+        },
+      },
+      { $match: { count: { $gte: 2 } } },
+      { $sort: { count: -1 } },
+    ]);
+
+    console.log(`found ${duplicates.length} duplicates`);
+    const excludedIds = duplicates.map(({ _id }) => _id);
+
     await paginator(
       AfFormation,
-      { filter: { matching_mna_formation: { $size: 1 }, matching_type: "3" }, lean: true, limit: 200 },
+      {
+        filter: {
+          matching_mna_formation: { $size: 1 },
+          matching_type: "3",
+          "matching_mna_formation._id": { $nin: excludedIds },
+        },
+        lean: true,
+        limit: 200,
+      },
       async (formation) => await reconciliationAffelnet(formation, "AUTOMATIQUE")
     );
 
