0b5b03e55ce9e545909ee93a13e325f618c081f4
PAblo <pablohanry9@gmail..com>
2021-05-04 13:53:11 +0200

    feat: starting facet

--- ui/src/pages/Search/components/index.js
+++ ui/src/pages/Search/components/index.js
@@ -1,9 +1,9 @@
 import Facet from "./Facet";
-import CardListFormation from "./CardListFormation";
-import CardListEtablissements from "./CardListEtablissements";
 import Pagination from "./Pagination";
 import ToggleCatalogue from "./ToggleCatalogue";
 import QueryBuilder from "./QueryBuilder";
 import ExportButton from "./ExportButton";
 
-export { Facet, CardListFormation, CardListEtablissements, Pagination, ToggleCatalogue, QueryBuilder, ExportButton };
+export { Facet, Pagination, ToggleCatalogue, QueryBuilder, ExportButton };
+export * from "./CardListEtablissements";
+export * from "./CardListFormation";
