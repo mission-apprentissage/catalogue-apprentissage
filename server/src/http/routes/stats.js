9e3baeee25c855f99844941e5aa557b489511c62
Kevin Barnoin <k.barnoin@gmail.com>
2021-05-05 16:32:17 +0200

    feat: gather stats from external source

--- server/src/http/routes/stats.js
+++ server/src/http/routes/stats.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const tryCatch = require("../middlewares/tryCatchMiddleware");
 const { getAllStats } = require("../../common/components/stats");
+const { Statistique } = require("../../common/model");
 
 module.exports = () => {
   const router = express.Router();
+
   router.get("/", async (req, res) => {
     const stats = await getAllStats();
-    res.json(stats);
+    return res.json(stats);
   });
+
+  router.post(
+    "/",
+    tryCatch(async (req, res) => {
+      const payload = {
+        source: req.source,
+        id_rco_formation: req.id_rco_formation,
+        date: new Date(),
+      };
+
+      await Statistique.create(payload);
+
+      return res.sendStatus(200);
+    })
+  );
 
   return router;
 };
