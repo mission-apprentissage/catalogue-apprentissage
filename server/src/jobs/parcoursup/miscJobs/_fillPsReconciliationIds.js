60fef610fc06ea5a1571ab6600544d1ae406c8d9
Gethi <bigard.antoine@gmail.com>
2021-05-18 17:27:16 +0200

    dummy fix

--- server/src/jobs/parcoursup/miscJobs/_fillPsReconciliationIds.js
+++ server/src/jobs/parcoursup/miscJobs/_fillPsReconciliationIds.js
@@ -12,11 +12,11 @@ async function migrate() {
   console.log(dataset.length);
 
   await asyncForEach(dataset, async (psFormation2021) => {
-    const { _id, id_reconciliation } = psFormation2021;
+    const { id_reconciliation } = psFormation2021; // _id
 
     let matching = await PsReconciliation.findOne({ _id: new mongoose.Types.ObjectId(id_reconciliation) });
 
-    matching._doc.ids_parcoursup = [...matching._doc.ids_parcoursup, _id];
+    matching._doc.ids_parcoursup = []; // [...matching._doc.ids_parcoursup, _id];
 
     matching.save();
   });
