9cd0b005707c45148f2bab6b68b24a761eebbd5f
Gethi <bigard.antoine@gmail.com>
2021-05-18 16:39:42 +0200

    feat: add ids_parcoursup to model psReconciliation

---
+++ server/src/jobs/parcoursup/miscJobs/_fillPsReconciliationIds.js
@@ -0,0 +1,23 @@
+const { runScript } = require("../../scriptWrapper");
+const { asyncForEach } = require("../../../common/utils/asyncUtils");
+const { PsReconciliation, PsFormation2021 } = require("../../../common/model");
+const mongoose = require("mongoose");
+
+runScript(async () => {
+  await migrate();
+});
+
+async function migrate() {
+  const dataset = await PsFormation2021.find({ id_reconciliation: { $ne: null } }).lean();
+  console.log(dataset.length);
+
+  await asyncForEach(dataset, async (psFormation2021) => {
+    const { _id, id_reconciliation } = psFormation2021;
+
+    let matching = await PsReconciliation.findOne({ _id: new mongoose.Types.ObjectId(id_reconciliation) });
+
+    matching._doc.ids_parcoursup = [...matching._doc.ids_parcoursup, _id];
+
+    matching.save();
+  });
+}
