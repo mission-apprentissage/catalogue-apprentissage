b126d24d98ab1f59f016916cd3323b148c4fb05c
Gethi <bigard.antoine@gmail.com>
2021-05-18 12:26:03 +0200

    feat: base useSearch

--- ui/src/common/components/Search/Search.js
+++ ui/src/common/components/Search/Search.js
@@ -1,12 +1,206 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useState, useCallback } from "react";
+import { ReactiveBase, ReactiveList, DataSearch, SingleList, SelectedFilters } from "@appbaseio/reactivesearch";
+import { Container, Flex, Box, Heading, Text, Spinner } from "@chakra-ui/react";
+import Switch from "react-switch";
+import useAuth from "../../hooks/useAuth";
+import { hasOneOfRoles } from "../../utils/rolesUtils";
+import {
+  QueryBuilder,
+  CardListFormation,
+  CardListEtablissements,
+  Facet,
+  ToggleCatalogue,
+  ExportButton,
+} from "./components";
+import constantsRcoFormations from "./constantsRCOFormations";
+import constantsEtablissements from "./constantsEtablissements";
 import "./search.css";
-import ResultsEs from "./Results";
+import queryString from "query-string";
+import { useSearch } from "../../hooks/useSearch";
 
-export default ({ match, location }) => {
+export default ({ match, location, context }) => {
+  const { defaultMode } = queryString.parse(location.search);
+  const [mode, setMode] = useState(defaultMode ?? "simple");
+  const [isCatalogEligible, setCatalogueEligible] = useState(true);
+
+  const { loaded, base, count, isBaseFormations, endpoint } = useSearch({ context });
+  console.log({ loaded, base, count, isBaseFormations, endpoint });
+
+  let [auth] = useAuth();
+
+  const { FILTERS, facetDefinition, queryBuilderField, dataSearch, columnsDefinition } = isBaseFormations
+    ? constantsRcoFormations
+    : constantsEtablissements;
+
+  const handleSearchSwitchChange = () => {
+    setMode((prevValue) => (prevValue === "simple" ? "advanced" : "simple"));
+  };
+
+  const resetCount = useCallback(
+    async (val) => {
+      try {
+        // let count = await countItems(base, val);
+        if (base === "etablissements") {
+          // setItemsCount(`${count} établissements`);
+        } else {
+          setCatalogueEligible(!!val);
+          // setItemsCount(`${count} formations au ${val ? "Catalogue général" : "Catalogue non-éligible"}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [base]
+  );
+
+  if (!loaded) {
+    return (
+      <Box className="search-page" h="full">
+        <Spinner />
+      </Box>
+    );
+  }
+
   return (
-    <Box className="search-page">
-      <ResultsEs match={match} location={location} />
+    <Box className="search-page" h="full">
+      <ReactiveBase url={`${endpoint}/es/search`} app={base}>
+        <SingleList
+          componentId="published"
+          dataField="published"
+          react={{ and: FILTERS }}
+          value={"true"}
+          defaultValue={"true"}
+          showFilter={false}
+          showSearch={false}
+          showCount={false}
+          render={() => {
+            return <div />;
+          }}
+        />
+        <div className="search">
+          <Container maxW="full">
+            <label className="react-switch" style={{ right: "70px" }}>
+              <Switch onChange={handleSearchSwitchChange} checked={mode !== "simple"} />
+              <Text as="span" textStyle="sm">
+                Recherche avancée
+              </Text>
+            </label>
+            <Heading as="h1" fontSize="beta" className="title">
+              {isBaseFormations
+                ? "Catalogue des formations en apprentissage 2021"
+                : "Liste des établissements de formation"}
+            </Heading>
+            <Flex className="search-row" flexDirection={["column", "row"]}>
+              <div className={`search-sidebar`}>
+                {isBaseFormations && <ToggleCatalogue filters={FILTERS} onChanged={resetCount} />}
+                {facetDefinition
+                  .filter(
+                    ({ roles, showCatalogEligibleOnly }) =>
+                      (!showCatalogEligibleOnly || isCatalogEligible) && (!roles || hasOneOfRoles(auth, roles))
+                  )
+                  .map((fd, i) => {
+                    return (
+                      <Facet
+                        key={i}
+                        componentId={fd.componentId}
+                        dataField={fd.dataField}
+                        title={fd.title}
+                        filterLabel={fd.filterLabel}
+                        selectAllLabel={fd.selectAllLabel}
+                        filters={FILTERS}
+                        sortBy={fd.sortBy}
+                      />
+                    );
+                  })}
+              </div>
+              <div className="search-results">
+                {mode !== "simple" && (
+                  <QueryBuilder
+                    lang="fr"
+                    collection={base}
+                    react={{ and: FILTERS.filter((e) => e !== "QUERYBUILDER") }}
+                    fields={queryBuilderField}
+                  />
+                )}
+                {mode === "simple" && (
+                  <div className={`search-container search-container-${mode}`}>
+                    <DataSearch
+                      componentId="SEARCH"
+                      placeholder={dataSearch.placeholder}
+                      fieldWeights={dataSearch.fieldWeights}
+                      dataField={dataSearch.dataField}
+                      autosuggest={true}
+                      queryFormat="and"
+                      size={20}
+                      showFilter={true}
+                      filterLabel="recherche"
+                      react={{ and: FILTERS.filter((e) => e !== "SEARCH") }}
+                    />
+                  </div>
+                )}
+                <Box pt={2}>
+                  <SelectedFilters showClearAll={false} innerClass={{ button: "selected-filters-button" }} />
+                </Box>
+                <div className={`result-view`}>
+                  <ReactiveList
+                    componentId="result"
+                    title="Results"
+                    dataField="_id"
+                    loader="Chargement des résultats.."
+                    size={8}
+                    pagination={true}
+                    showEndPage={false}
+                    showResultStats={true}
+                    sortBy="asc"
+                    defaultQuery={() => {
+                      return {
+                        _source: columnsDefinition.map(({ accessor }) => accessor),
+                      };
+                    }}
+                    renderItem={(data) =>
+                      isBaseFormations ? (
+                        <CardListFormation data={data} key={data._id} />
+                      ) : (
+                        <CardListEtablissements data={data} key={data._id} />
+                      )
+                    }
+                    renderResultStats={(stats) => {
+                      return (
+                        <div className="summary-stats">
+                          <span className="summary-text">
+                            {/* {isBaseFormations
+                              ? `${stats.numberOfResults.toLocaleString("fr-FR")} formations`
+                              : `${stats.numberOfResults} établissements affichées sur ${itemsCount}`} */}
+                          </span>
+                          {auth?.sub !== "anonymous" && (
+                            <ExportButton
+                              index={base}
+                              filters={FILTERS}
+                              columns={columnsDefinition
+                                .filter((def) => !def.debug)
+                                .map((def) => ({
+                                  header: def.Header,
+                                  fieldName: def.accessor,
+                                  formatter: def.formatter,
+                                }))}
+                              defaultQuery={{
+                                match: {
+                                  published: true,
+                                },
+                              }}
+                            />
+                          )}
+                        </div>
+                      );
+                    }}
+                    react={{ and: FILTERS }}
+                  />
+                </div>
+              </div>
+            </Flex>
+          </Container>
+        </div>
+      </ReactiveBase>
     </Box>
   );
 };
---
+++ ui/src/common/hooks/useSearch.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { _get } from "../httpClient";
+
+const FORMATIONS_ES_INDEX = "convertedformation";
+const ETABLISSEMENTS_ES_INDEX = "etablissements";
+
+const CATALOGUE_API_ENDPOINT =
+  process.env.REACT_APP_ENDPOINT_NEW_FRONT || "https://catalogue-recette.apprentissage.beta.gouv.fr/api";
+const TCO_API_ENDPOINT =
+  process.env.REACT_APP_ENDPOINT_TCO || "https://tables-correspondances.apprentissage.beta.gouv.fr/api";
+
+const getEsBase = (context) => {
+  if (context === "organismes") {
+    return ETABLISSEMENTS_ES_INDEX;
+  }
+  return FORMATIONS_ES_INDEX;
+};
+
+const getCountEntities = async (base, etablissement_reference_catalogue_published = true) => {
+  if (base === ETABLISSEMENTS_ES_INDEX) {
+    const params = new window.URLSearchParams({
+      query: JSON.stringify({ published: true }),
+    });
+    return await _get(`${TCO_API_ENDPOINT}/entity/etablissements/count?${params}`, false);
+  }
+
+  const params = new window.URLSearchParams({
+    query: JSON.stringify({ published: true, etablissement_reference_catalogue_published }),
+  });
+  return await _get(`${CATALOGUE_API_ENDPOINT}/entity/formations2021/count?${params}`, false);
+};
+
+// context: organismes | catalogue_general | catalogue_non_eligible
+export function useSearch({ context }) {
+  const base = getEsBase(context);
+  const isBaseFormations = base === FORMATIONS_ES_INDEX;
+  const endpoint = isBaseFormations ? CATALOGUE_API_ENDPOINT : TCO_API_ENDPOINT;
+  const [searchState, setSearchState] = useState({
+    loaded: false,
+    base,
+    count: 0,
+    isBaseFormations,
+    endpoint,
+  });
+
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const abortController = new AbortController();
+    getCountEntities(base)
+      .then((countEntities) => {
+        if (!abortController.signal.aborted) {
+          setSearchState({ loaded: true, base, count: countEntities, isBaseFormations, endpoint });
+        }
+      })
+      .catch((e) => {
+        if (!abortController.signal.aborted) {
+          setError(e);
+        }
+      });
+    return () => {
+      abortController.abort();
+    };
+  }, [base, endpoint, isBaseFormations]);
+
+  if (error !== null) {
+    throw error;
+  }
+
+  return searchState;
+}
