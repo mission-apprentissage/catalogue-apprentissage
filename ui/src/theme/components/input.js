e16518f842acb27ac2931389729f0ede5ba1082a
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-05-20 15:33:45 +0200

    chore(ui): login forms new design (#325)

--- ui/src/theme/components/input.js
+++ ui/src/theme/components/input.js
@@ -11,6 +11,16 @@ const Input = {
         borderColor: "bluefrance",
       },
     },
+    outline: {
+      field: {
+        borderBottomRadius: 0,
+        borderWidth: 0,
+        borderBottom: "2px solid",
+        marginBottom: "-2px",
+        borderColor: "grey.600",
+        bg: "grey.200",
+      },
+    },
   },
 };
 
