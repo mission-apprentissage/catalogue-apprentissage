dbd4edaafa28da1e86d728b5d6fe3c68bed7b946
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-06-02 10:12:45 +0200

    chore(ui): add cookies paragraph

--- ui/src/pages/legal/Cookies.jsx
+++ ui/src/pages/legal/Cookies.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, Link, Text } from "@chakra-ui/react";
 import { Breadcrumb } from "../../common/components/Breadcrumb";
 import Layout from "../layout/Layout";
 import { setTitle } from "../../common/utils/pageUtils";
+import { ExternalLinkLine } from "../../theme/components/icons";
 
 export default () => {
   const title = "Gestion des Cookies";
@@ -13,10 +14,31 @@ export default () => {
       <Box w="100%" pt={[4, 8]} px={[1, 1, 12, 24]}>
         <Container maxW="xl">
           <Breadcrumb pages={[{ title: "Accueil", to: "/" }, { title: title }]} />
-          <Heading textStyle="h2" color="grey.800" mt={5}>
+          <Heading textStyle="h2" mt={5}>
             {title}
           </Heading>
-          <Box mt={4}></Box>
+          <Box pt={4} pb={16}>
+            <Text>
+              Pour les utilisateurs connectés, ce site dépose un petit fichier texte (un « cookie ») sur votre
+              ordinateur pour vous authentifier et garder votre session active.
+              <br />
+              Cela nous permet de vous laisser connecté même si vous consultez le site dans un nouvel onglet, et de vous
+              éviter de vous reconnecter à chaque fois que vous visitez le site.
+              <br />
+              <br />
+              Ce site n’affiche pas de bannière de consentement aux cookies car les cookies d'authentification en sont
+              exemptés, cf.{" "}
+              <Link
+                href={"https://www.cnil.fr/fr/cookies-et-traceurs-que-dit-la-loi"}
+                textDecoration={"underline"}
+                isExternal
+              >
+                https://www.cnil.fr/fr/cookies-et-traceurs-que-dit-la-loi{" "}
+                <ExternalLinkLine w={"0.75rem"} h={"0.75rem"} mb={"0.125rem"} />
+              </Link>
+              .
+            </Text>
+          </Box>
         </Container>
       </Box>
     </Layout>
