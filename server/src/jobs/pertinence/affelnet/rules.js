c7662b3dec554ce26be725a8ac4c43b0b474870b
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-06-03 14:59:02 +0200

    feat(affelnet): check cfd expiracy in pertinence rules (#342)

--- server/src/jobs/pertinence/affelnet/rules.js
+++ server/src/jobs/pertinence/affelnet/rules.js
@@ -1,5 +1,13 @@
 const { toBePublishedRules } = require("../../common/utils/referenceUtils");
 
+const getCfdExpireRule = (duration) => {
+  const currentYear = new Date().getFullYear();
+  const limitYear = currentYear + duration - 1;
+  return {
+    $or: [{ cfd_date_fermeture: { $gt: new Date(`${limitYear}-12-31T00:00:00.000Z`) } }, { cfd_date_fermeture: null }],
+  };
+};
+
 const getMefRule = (...args) => {
   const rule = args.reduce((acc, regex) => {
     return [...acc, { mef_10_code: { $regex: regex } }, { "bcn_mefs_10.mef10": { $regex: regex } }];
@@ -17,51 +25,51 @@ const aPublierSoumisAValidationRules = {
           $or: [
             {
               ...getMefRule(/11$/),
-              $and: [getMefRule(/^240/)],
+              $and: [getMefRule(/^240/), getCfdExpireRule(1)],
             },
             {
               ...getMefRule(/31$/),
-              $and: [getMefRule(/^242/)],
+              $and: [getMefRule(/^242/), getCfdExpireRule(3)],
             },
           ],
         },
         {
           diplome: "BAC PROFESSIONNEL",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^246/)],
+          $and: [getMefRule(/^246/), getCfdExpireRule(2)],
         },
         {
           diplome: "BAC PROFESSIONNEL AGRICOLE",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^273/)],
+          $and: [getMefRule(/^273/), getCfdExpireRule(2)],
         },
 
         {
           diplome: "BREVET PROFESSIONNEL",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^254/)],
+          $and: [getMefRule(/^254/), getCfdExpireRule(2)],
         },
         {
           diplome: "BREVET PROFESSIONNEL AGRICOLE",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^254/)],
+          $and: [getMefRule(/^254/), getCfdExpireRule(2)],
         },
         {
           diplome: "BREVET DES METIERS D'ART - BREVET DES METIERS DU SPECTACLE",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^251/)],
+          $and: [getMefRule(/^251/), getCfdExpireRule(2)],
         },
         {
           diplome: "MENTION COMPLEMENTAIRE",
           niveau: "3 (CAP...)",
           ...getMefRule(/11$/),
-          $and: [getMefRule(/^253/)],
+          $and: [getMefRule(/^253/), getCfdExpireRule(1)],
         },
         {
           diplome: "MENTION COMPLEMENTAIRE AGRICOLE",
           niveau: "3 (CAP...)",
           ...getMefRule(/11$/),
-          $and: [getMefRule(/^274/)],
+          $and: [getMefRule(/^274/), getCfdExpireRule(1)],
         },
       ],
     },
@@ -79,22 +87,22 @@ const aPublierRules = {
         {
           diplome: "CERTIFICAT D'APTITUDES PROFESSIONNELLES",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^241/)],
+          $and: [getMefRule(/^241/), getCfdExpireRule(2)],
         },
         {
           diplome: "CERTIFICAT D'APTITUDES PROFESSIONNELLES AGRICOLES",
           ...getMefRule(/21$/),
-          $and: [getMefRule(/^271/)],
+          $and: [getMefRule(/^271/), getCfdExpireRule(2)],
         },
         {
           diplome: "BAC PROFESSIONNEL",
           ...getMefRule(/31$/),
-          $and: [getMefRule(/^247/)],
+          $and: [getMefRule(/^247/), getCfdExpireRule(3)],
         },
         {
           diplome: "BAC PROFESSIONNEL AGRICOLE",
           ...getMefRule(/31$/),
-          $and: [getMefRule(/^276/)],
+          $and: [getMefRule(/^276/), getCfdExpireRule(3)],
         },
       ],
     },
