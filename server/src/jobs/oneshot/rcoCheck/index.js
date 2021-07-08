b1da147f40b32182dfa784e508447505576e7ebc
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-07-08 14:54:24 +0200

    feat: Check RCO data : find geoloc gaps (#363)

--- server/src/jobs/oneshot/rcoCheck/index.js
+++ server/src/jobs/oneshot/rcoCheck/index.js
@@ -40,6 +40,25 @@ const findMismatch = async (totalFormationsMna, key, rcoKey) => {
   console.log(`total ${key} changés :`, total, `(${((total / totalFormationsMna) * 100).toFixed(2)}%)`);
 };
 
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+function distanceInMeterBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  const earthRadiusKm = 6371;
+
+  const dLat = degreesToRadians(lat2 - lat1);
+  const dLon = degreesToRadians(lon2 - lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return earthRadiusKm * c * 1000;
+}
+
 runScript(async () => {
   logger.info(`Start check RCO data`);
 
@@ -59,4 +78,28 @@ runScript(async () => {
   await findMismatch(totalFormationsMna, "etablissement_gestionnaire_uai");
   await findMismatch(totalFormationsMna, "etablissement_formateur_siret");
   await findMismatch(totalFormationsMna, "etablissement_formateur_uai");
+
+  const distance = 1000;
+  const pool = await ConvertedFormation.find(
+    {
+      published: true,
+      lieu_formation_geo_coordonnees: { $ne: null },
+      idea_geo_coordonnees_etablissement: { $ne: null },
+    },
+    { id_rco_formation: 1, lieu_formation_geo_coordonnees: 1, idea_geo_coordonnees_etablissement: 1 }
+  ).lean();
+
+  const filteredPool = pool.filter(({ lieu_formation_geo_coordonnees, idea_geo_coordonnees_etablissement }) => {
+    const [ideaLat, ideaLon] = idea_geo_coordonnees_etablissement.split(",");
+    const [lat, lon] = lieu_formation_geo_coordonnees.split(",");
+    return (
+      distanceInMeterBetweenEarthCoordinates(Number(ideaLat), Number(ideaLon), Number(lat), Number(lon)) > distance
+    );
+  });
+
+  console.log(
+    `total geoloc changées (éloignées de plus de ${distance}m) :`,
+    filteredPool.length,
+    `(${((filteredPool.length / totalFormationsMna) * 100).toFixed(2)}%)`
+  );
 });
