12851b047ae7dd1c42118bf43d91cf27008b50a4
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-09-22 17:34:17 +0200

    chore: add unit tests on utils

--- server/src/logic/mappers/etablissementsMapper__test.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-const { etablissementsMapper } = require("./etablissementsMapper");
-
-describe(__filename, () => {
-  it("Doit ajouter les informations relative à un cfd à un objet formation", async () => {
-    assert.deepStrictEqual(await etablissementsMapper(), null);
-  });
-});
