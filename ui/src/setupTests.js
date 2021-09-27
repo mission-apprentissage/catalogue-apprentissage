3977c44299f11ba4879b4e66cf54f43b7109252f
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-09-27 16:23:35 +0200

    tests: some tests on Perimetre page (#414)

--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+import { setLogger } from "react-query";
+
+setLogger({
+  log: console.log,
+  warn: console.warn,
+  // ✅ no more errors on the console
+  error: () => {},
+});
