f4be70393cc4c276d90668697de937beca694eb8
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-09-27 16:52:17 +0200

    Fix/reconciliation count (#415)
    * fix: sync es for reconciliaton count
    
    * chore: rm unused files

--- ui/src/pages/reconciliation-parcoursup-depracated-toremove-soon/components/index.js
+++
@@ -1,6 +0,0 @@
-import Layout from "./Layout";
-import Accordion from "./Accordion";
-import Loading from "./Loading";
-import addEtablissement from "./addEtablissement";
-
-export { Layout, Accordion, Loading, addEtablissement };
