156509e8f222df06b01d681f3dc83f373fa54674
antoinebigard <bigard.antoine@gmail.com>
2021-10-19 18:07:25 +0200

    chore: bump version

--- ui/src/CHANGELOG.js
+++ ui/src/CHANGELOG.js
@@ -12,6 +12,15 @@ const changelog = {
       improvements: ["Travaux sur le flux retour vers les Carif-Oref et gestion des modifications"],
     },
     {
+      version: "4.8.1",
+      date: "Le 19 Octobre 2021",
+      about: `<h4>À propos de cette version</h4>
+      Cette version porte des améliorations et quelques correctifs qui sont détaillés ci après.`,
+      fixes: [],
+      features: [],
+      improvements: ["[Rapprochement Parcoursup] Pré lancement campagne"],
+    },
+    {
       version: "4.8.0",
       date: "Le 19 Octobre 2021",
       about: `<h4>À propos de cette version</h4>
