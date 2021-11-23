565fbc15df4e7bc511797f2e6ff155b00bf4d173
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-11-23 14:02:15 +0100

    fix: Download CSV content type

--- ui/src/common/utils/downloadUtils.js
+++ ui/src/common/utils/downloadUtils.js
@@ -1,14 +1,15 @@
 export const downloadCSV = (fileName, csv) => {
-  let blob = new Blob([csv]);
+  const blob = new Blob([csv], {
+    type: "text/csv;charset=UTF-8",
+  });
 
   if (window.navigator.msSaveOrOpenBlob) {
     // IE hack; see http://msdn.microsoft.com/en-us/library/ie/hh779016.aspx
     window.navigator.msSaveBlob(blob, fileName);
   } else {
     let a = window.document.createElement("a");
-    a.href = window.URL.createObjectURL(blob, {
-      type: "text/plain;charset=UTF-8",
-    });
+    a.href = window.URL.createObjectURL(blob);
+
     a.download = fileName;
     document.body.appendChild(a);
     a.click(); // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
