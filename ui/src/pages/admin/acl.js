e7515e5e42f671198ec66acdc4021e0df8ea3f6f
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-12-06 12:19:36 +0100

    fix acl

--- ui/src/pages/admin/acl.js
+++ ui/src/pages/admin/acl.js
@@ -21,10 +21,6 @@ export default [
       },
       {
         feature: "Voir les statuts de publication parcourSup",
-        ref: "page_catalogue/voir_status_publication",
-      },
-      {
-        feature: "Voir les statuts de publication parcourSup",
         ref: "page_catalogue/voir_status_publication_ps",
       },
       {
