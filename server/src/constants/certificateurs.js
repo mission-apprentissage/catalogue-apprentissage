1cd67259d039a1c0532d63d5c1f9e1a64b972302
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-01-04 09:48:31 +0100

    fix: add ministere du travail label to default habilite list

--- server/src/constants/certificateurs.js
+++ server/src/constants/certificateurs.js
@@ -2,6 +2,7 @@ const habiliteList = [
   "Ministère du travail",
   "Ministère chargé de l'Emploi",
   "Ministère du Travail - Délégation Générale à l'Emploi et à la Formation Professionnelle (DGEFP)",
+  "MINISTERE DU TRAVAIL DE L'EMPLOI ET DE L'INSERTION",
 ];
 
 module.exports = {
--- ui/src/constants/certificateurs.js
+++ ui/src/constants/certificateurs.js
@@ -2,6 +2,7 @@ const HABILITE_LIST = [
   "Ministère du travail",
   "Ministère chargé de l'Emploi",
   "Ministère du Travail - Délégation Générale à l'Emploi et à la Formation Professionnelle (DGEFP)",
+  "MINISTERE DU TRAVAIL DE L'EMPLOI ET DE L'INSERTION",
 ];
 
 export { HABILITE_LIST };
