cadabecf48b5a29855d118e0b21c352b22c7b0e7
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-01-05 09:45:17 +0100

    fix: Shrink reports size

--- server/tests/unit/common/utils/reportUtils.test.js
+++ server/tests/unit/common/utils/reportUtils.test.js
@@ -22,31 +22,31 @@ describe(__filename, () => {
     assert.strictEqual(count, 0);
   });
 
-  it("should create 1 report only under 500 data", async () => {
+  it("should create 1 report only under 250 data", async () => {
+    const date = Date.now();
+    const type = "rcoImport";
+
+    await storeByChunks(type, date, { title: "hello" }, "added", [...Array(250).keys()]);
+
+    const count = await Report.countDocuments({});
+    assert.strictEqual(count, 1);
+  });
+
+  it("should create 2 reports because of chunks after 250 data", async () => {
     const date = Date.now();
     const type = "rcoImport";
 
     await storeByChunks(type, date, { title: "hello" }, "added", [...Array(500).keys()]);
 
     const count = await Report.countDocuments({});
-    assert.strictEqual(count, 1);
-  });
-
-  it("should create 2 reports because of chunks after 500 data", async () => {
-    const date = Date.now();
-    const type = "rcoImport";
-
-    await storeByChunks(type, date, { title: "hello" }, "added", [...Array(1000).keys()]);
-
-    const count = await Report.countDocuments({});
     assert.strictEqual(count, 2);
   });
 
-  it("should create 3 reports because of chunks after 1000 data", async () => {
+  it("should create 3 reports because of chunks after 500 data", async () => {
     const date = Date.now();
     const type = "rcoImport";
 
-    await storeByChunks(type, date, { title: "hello" }, "added", [...Array(1001).keys()]);
+    await storeByChunks(type, date, { title: "hello" }, "added", [...Array(501).keys()]);
 
     const count = await Report.countDocuments({});
     assert.strictEqual(count, 3);
