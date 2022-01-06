9e6aacc7c957eb4410fed19fb56ee39c1121974c
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-01-06 15:28:31 +0100

    refactor: way faster reports using streams

--- server/src/http/routes/report.js
+++ server/src/http/routes/report.js
@@ -1,38 +1,23 @@
 const express = require("express");
 const tryCatch = require("../middlewares/tryCatchMiddleware");
 const { Report } = require("../../common/model");
+const { oleoduc, transformIntoJSON } = require("oleoduc");
+const { sendJsonStream } = require("../../common/utils/httpUtils");
 
 module.exports = () => {
   const router = express.Router();
 
   /**
-   * Get Report /report GET
-   */
-  router.get(
-    "/report",
-    tryCatch(async (req, res) => {
-      const { type, date } = req.query;
-
-      const report = await Report.findOne({ type, date });
-      if (report) {
-        return res.json(report);
-      } else {
-        res.status(404).send({ message: "Item doesn't exist" });
-      }
-    })
-  );
-
-  /**
-   * Get Reports /reports GET
+   * Get Reports /reports.ndjson GET
    */
   router.get(
     "/reports",
     tryCatch(async (req, res) => {
-      const { type, date, minDate, maxDate, page = 1, uuidReport } = req.query;
-      let data = null;
+      const { type, date, minDate, maxDate, uuidReport } = req.query;
+      let filter;
 
       if (minDate && maxDate) {
-        const filter =
+        filter =
           uuidReport !== "null"
             ? {
                 type,
@@ -42,30 +27,16 @@ module.exports = () => {
                 type,
                 date: { $gte: new Date(minDate), $lt: new Date(maxDate) },
               };
-        data = await Report.paginate(filter, { page, limit: 1 });
       } else {
-        const filter = { type, date };
+        filter = { type, date };
         if (uuidReport !== "null") {
           filter.uuid = uuidReport;
         }
-        data = await Report.paginate(filter, { page, limit: 1 });
       }
 
-      if (data?.docs?.[0]) {
-        const report = data.docs[0];
+      let stream = oleoduc(Report.find(filter).cursor(), transformIntoJSON());
 
-        return res.json({
-          report,
-          pagination: {
-            page: data.page,
-            resultats_par_page: 1,
-            nombre_de_page: data.pages,
-            total: data.total,
-          },
-        });
-      } else {
-        res.status(404).send({ message: "Item doesn't exist" });
-      }
+      return sendJsonStream(stream, res);
     })
   );
 
