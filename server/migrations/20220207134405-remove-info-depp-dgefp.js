896d064da5754b1519f1ab135b02f6067fb8c5ea
Quentin Petel <contact@quentinpetel.fr>
2022-02-08 16:53:00 +0100

    Add migration to remove conventionnement related fields in formation

--- server/migrations/20220207134405-remove-info-depp-dgefp.js
+++ server/migrations/20220207134405-remove-info-depp-dgefp.js
@@ -1,10 +1,33 @@
 module.exports = {
   async up(db) {
-    const collection = db.collection("etablissements");
+    const etablissements = db.collection("etablissements");
+    const formations = db.collection("formations");
 
-    console.log(`Removing info_depp, info_depp_info, info_dgefp, info_dgefp_info fields`);
+    console.log(`Remove info_depp, info_depp_info, info_dgefp, info_dgefp_info fields from 'etablissement'`);
 
-    await collection.updateMany({}, [{ $unset: ["info_depp", "info_depp_info", "info_dgefp", "info_dgefp_info"] }]);
+    await etablissements.updateMany({}, [{ $unset: ["info_depp", "info_depp_info", "info_dgefp", "info_dgefp_info"] }]);
+
+    console.log(
+      `Remove etablissement_formateur_type, etablissement_formateur_conventionne, etablissement_formateur_declare_prefecture, \
+etablissement_gestionnaire_type, etablissement_gestionnaire_conventionne, etablissement_gestionnaire_declare_pr  efecture, \
+etablissement_reference_type, etablissement_reference_conventionne, etablissement_reference_declare_prefecture fields from 'formations'`
+    );
+
+    await formations.updateMany({}, [
+      {
+        $unset: [
+          "etablissement_formateur_type",
+          "etablissement_formateur_conventionne",
+          "etablissement_formateur_declare_prefecture",
+          "etablissement_gestionnaire_type",
+          "etablissement_gestionnaire_conventionne",
+          "etablissement_gestionnaire_declare_prefecture",
+          "etablissement_reference_type",
+          "etablissement_reference_conventionne",
+          "etablissement_reference_declare_prefecture",
+        ],
+      },
+    ]);
   },
 
   async down() {
