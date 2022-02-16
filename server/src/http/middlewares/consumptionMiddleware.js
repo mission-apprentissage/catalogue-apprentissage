36e152077f2ef3889e40099287104c1cb19ae866
Quentin Petel <contact@quentinpetel.fr>
2022-02-16 11:53:19 +0100

    fix: better url params handling

--- server/src/http/middlewares/consumptionMiddleware.js
+++ server/src/http/middlewares/consumptionMiddleware.js
@@ -1,44 +1,39 @@
 const logger = require("../../common/logger");
 const { Consumption } = require("../../common/model");
 
-module.exports = () => {
-  return async (req, res, next) => {
+module.exports = async (req) => {
+  try {
+    const ip = req.headers["x-forwarded-for"]?.split(",").shift() || req.socket?.remoteAddress;
+    const route = req.route ? `${req.baseUrl}${req.route?.path}` : req.url.split("?")[0];
+    const method = req.method;
+
     try {
-      const ip = req.headers["x-forwarded-for"]?.split(",").shift() || req.socket?.remoteAddress;
-      const route = req.url.split("?")[0];
-      const method = req.method;
-
-      try {
-        await Consumption.findOneAndUpdate(
-          { route, method, "consumers.ip": ip },
-          {
-            $inc: { globalCallCount: 1, "consumers.$.callCount": 1 },
-          },
-          {
-            upsert: true,
-            new: false,
-          }
-        );
-      } catch (error) {
-        await Consumption.findOneAndUpdate(
-          {
-            route,
-            method,
-          },
-          {
-            $inc: { globalCallCount: 1 },
-            $push: { consumers: { ip, callCount: 1 } },
-          },
-          {
-            upsert: true,
-          }
-        );
-      }
-
-      next();
+      await Consumption.findOneAndUpdate(
+        { route, method, "consumers.ip": ip },
+        {
+          $inc: { globalCallCount: 1, "consumers.$.callCount": 1 },
+        },
+        {
+          upsert: true,
+          new: false,
+        }
+      );
     } catch (error) {
-      logger.error(error, "Error while collecting endpoint consumption.");
-      next(error);
+      await Consumption.findOneAndUpdate(
+        {
+          route,
+          method,
+        },
+        {
+          $inc: { globalCallCount: 1 },
+          $push: { consumers: { ip, callCount: 1 } },
+        },
+        {
+          upsert: true,
+        }
+      );
     }
-  };
+  } catch (error) {
+    logger.error(error, "Error while collecting endpoint consumption.");
+  }
 };
