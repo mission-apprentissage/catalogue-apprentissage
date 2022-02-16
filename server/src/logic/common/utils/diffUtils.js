427f139a3dbb42bf931d9ca1d35fd9759195fb0d
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-02-16 09:41:00 +0100

    chore: clean unused fields (#521)

--- server/src/logic/common/utils/diffUtils.js
+++ server/src/logic/common/utils/diffUtils.js
@@ -34,16 +34,13 @@ const diffFormation = (previousFormationP, nextFormationP) => {
 /*
  * Build updates history
  */
-const buildUpdatesHistory = (formation, updates, keys, source) => {
+const buildUpdatesHistory = (formation, updates, keys) => {
   const from = keys.reduce((acc, key) => {
     acc[key] = formation[key];
     return acc;
   }, {});
 
-  return [
-    ...formation.updates_history,
-    { from, to: { ...updates }, updated_at: Date.now(), ...(source ? { source } : {}) },
-  ];
+  return [...formation.updates_history, { from, to: { ...updates }, updated_at: Date.now() }];
 };
 
 module.exports = { diffFormation, buildUpdatesHistory };
