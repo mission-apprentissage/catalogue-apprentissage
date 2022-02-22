bb391415d8a960b8f65ad4b82eb99452a43dd2f8
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-02-22 14:05:46 +0100

    feature: Add automatic job for UAIs check (#529)
    * feature: Add automatic job for UAIs check
    
    * Move checkUai job from oneShot folder

---
+++ server/src/jobs/checkUai/index.js
@@ -0,0 +1,42 @@
+const { isValideUAI } = require("@mission-apprentissage/tco-service-node");
+const { runScript } = require("../scriptWrapper");
+const { Etablissement, Formation } = require("../../common/model");
+
+const updateUaiValidity = async (collection, uaiField, uaiValidityField) => {
+  console.info(`Checking for UAI in collection...`);
+  const cursor = await collection.find().cursor();
+  let count = 0;
+
+  for await (const entry of cursor) {
+    const valid = !entry[uaiField] || (await isValideUAI(entry[uaiField]));
+    !valid && console.log(`❌ ${entry[uaiField]}`);
+    !valid && count++;
+
+    await collection.updateOne(
+      { _id: entry._id },
+      {
+        $set: {
+          [uaiValidityField]: valid,
+        },
+      }
+    );
+  }
+  console.info(`${count} UAI not valid !`);
+  console.info(`Checking for UAI in collection: 🆗`);
+  console.info(`----------------------------------`);
+};
+
+const checkUai = async () => {
+  try {
+    await updateUaiValidity(Etablissement, "uai", "uai_valide");
+    await updateUaiValidity(Formation, "uai_formation", "uai_formation_valide");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = checkUai;
+
+if (process.env.standalone) {
+  runScript(async () => await checkUai());
+}
