e95a14cf409ac7149b599961611cb36988e598fc
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-02-24 16:03:15 +0100

    feat: Ajout du point GPS calculé pour une formation et calcul de la distance avec la valeur transmise (#536)
    * feat: Ajout du point GPS calculé pour une formation et calcul de la distance avec la valeur transmise
    
    * Remove logs
    
    * Check for fields in mongo query
    
    * Fix query

--- server/src/common/model/schema/formation/formation.js
+++ server/src/common/model/schema/formation/formation.js
@@ -458,6 +458,16 @@ const formationSchema = {
     implicit_type: "geo_point",
     description: "Latitude et longitude du lieu de formation",
   },
+  lieu_formation_geo_coordonnees_computed: {
+    type: String,
+    implicit_type: "geo_point",
+    description: "Latitude et longitude du lieu de formation déduit de l'adresse du flux RCO",
+  },
+  distance: {
+    type: Number,
+    default: null,
+    description: "Distance entre les coordonnées transmises et déduites à partir de l'adresse",
+  },
   lieu_formation_adresse: {
     index: true,
     type: String,
@@ -467,7 +477,7 @@ const formationSchema = {
   lieu_formation_adresse_computed: {
     type: String,
     default: null,
-    description: "Adresse du lieu de formation déduit de la géolocalisation le flux RCO",
+    description: "Adresse du lieu de formation déduit de la géolocalisation du flux RCO",
   },
   lieu_formation_siret: {
     type: String,
---
+++ server/src/jobs/oneshot/computeDistance/index.js
@@ -0,0 +1,69 @@
+const { getCoordinatesFromAddressData } = require("@mission-apprentissage/tco-service-node");
+const { distanceBetweenCoordinates } = require("../../../common/utils/distanceUtils");
+const { Formation } = require("../../../common/model");
+const { runScript } = require("../../scriptWrapper");
+
+const parseErrors = (messages) => {
+  if (!messages) {
+    return "";
+  }
+  return Object.entries(messages)
+    .filter(([key, value]) => key === "error" || `${value}`.toLowerCase().includes("erreur"))
+    .reduce((acc, [key, value]) => `${acc}${acc ? " " : ""}${key}: ${value}.`, "");
+};
+
+const computeDistance = async () => {
+  let count = 0;
+  const cursor = await Formation.find({
+    published: true,
+    lieu_formation_geo_coordonnees: { $ne: null },
+    lieu_formation_adresse: { $ne: null },
+    localite: { $ne: null },
+    code_postal: { $ne: null },
+    code_commune_insee: { $ne: null },
+  }).cursor();
+  for await (const formation of cursor) {
+    const addressData = {
+      nom_voie: formation.lieu_formation_adresse,
+      localite: formation.localite,
+      code_postal: formation.code_postal,
+      code_insee: formation.code_commune_insee,
+    };
+
+    try {
+      const { result: coordinates, messages } = await getCoordinatesFromAddressData(addressData);
+
+      const geolocError = parseErrors(messages);
+      if (!geolocError && coordinates.geo_coordonnees) {
+        const lieu_formation_geo_coordonnees_computed = coordinates.geo_coordonnees;
+
+        const [lat1, lon1] = lieu_formation_geo_coordonnees_computed.split(",");
+        const [lat2, lon2] = formation.lieu_formation_geo_coordonnees.split(",");
+
+        const distance = await distanceBetweenCoordinates(lat1, lon1, lat2, lon2);
+
+        await Formation.updateOne(
+          { _id: formation._id },
+          {
+            $set: {
+              lieu_formation_geo_coordonnees_computed,
+              distance,
+            },
+          }
+        );
+      }
+      if (++count % 100 === 0) {
+        console.error(`${count} distance computed.`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  console.info(`> ${count} formations traitées.`);
+};
+
+module.exports = computeDistance;
+
+if (process.env.standalone) {
+  runScript(async () => await computeDistance());
+}
