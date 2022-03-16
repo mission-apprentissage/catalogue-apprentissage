0e71952ebec7182e411ae07e49a8f61ac7decdec
Quentin Petel <contact@quentinpetel.fr>
2022-03-16 11:35:38 +0100

    Fix tests

--- ui/src/common/components/formation/StatutHistoryBlock.test.js
+++ ui/src/common/components/formation/StatutHistoryBlock.test.js
@@ -75,14 +75,6 @@ const formation = {
     },
     {
       parcoursup_statut: "en attente de publication",
-      date: "2022-01-26T06:18:19.222Z",
-    },
-    {
-      parcoursup_statut: "en attente de publication",
-      date: "2022-01-27T06:21:26.357Z",
-    },
-    {
-      parcoursup_statut: "en attente de publication",
       date: "2022-01-28T05:24:27.713Z",
     },
     {
@@ -166,11 +158,11 @@ test("display affelnet statut history", async () => {
 
   await waitFor(() => getByText(/Affelnet/));
 
-  expect(queryByText("31/01/2022")).toBeInTheDocument();
-  expect(queryByText("29/12/2021")).toBeInTheDocument();
-  expect(queryByText("12/06/2021")).toBeInTheDocument();
+  expect(queryByText(new Date("2022-01-31T09:46:33.274Z").toLocaleDateString())).toBeInTheDocument();
+  expect(queryByText(new Date("2021-12-29T03:39:55.777Z").toLocaleDateString())).toBeInTheDocument();
+  expect(queryByText(new Date("2021-06-12T02:16:15.664Z").toLocaleDateString())).toBeInTheDocument();
 
-  expect(queryByText("05/03/2022")).not.toBeInTheDocument();
+  expect(queryByText(new Date("2022-03-05T05:52:22.501Z").toLocaleDateString())).not.toBeInTheDocument();
 });
 
 test("display parcoursup statut history", async () => {
@@ -180,10 +172,10 @@ test("display parcoursup statut history", async () => {
 
   await waitFor(() => getByText(/Parcoursup/));
 
-  expect(queryByText("24/01/2022")).toBeInTheDocument();
-  expect(queryByText("10/12/2021")).toBeInTheDocument();
-  expect(queryByText("27/09/2021")).toBeInTheDocument();
-  expect(queryByText("19/07/2021")).toBeInTheDocument();
+  expect(queryByText(new Date("2022-01-24T05:17:57.784Z").toLocaleDateString())).toBeInTheDocument();
+  expect(queryByText(new Date("2021-12-10T04:19:37.114Z").toLocaleDateString())).toBeInTheDocument();
+  expect(queryByText(new Date("2021-09-27T00:56:33.397Z").toLocaleDateString())).toBeInTheDocument();
+  expect(queryByText(new Date("2021-07-19T02:11:01.285Z").toLocaleDateString())).toBeInTheDocument();
 
-  expect(queryByText("05/03/2022")).not.toBeInTheDocument();
+  expect(queryByText(new Date("2022-03-05T05:52:22.501Z").toLocaleDateString())).not.toBeInTheDocument();
 });
