df35e0d9ac5f2b62d0edb8d58ed8eed10c65864e
Eric Plaquevent <eric.plaquevent@gmail.com>
2022-03-18 11:24:49 +0100

    fix: format swagger files with prettier

--- server/src/jobs/doc/generateSwaggerSchema.js
+++ server/src/jobs/doc/generateSwaggerSchema.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const schemas = require("../../common/model/schema");
 const { getJsonFromMongooseSchema } = require("./mongooseUtils");
+const prettier = require("prettier");
+const packageJson = require("../../../package.json");
 
 const generateSwaggerSchema = () => {
   Object.keys(schemas).forEach((schemaName) => {
@@ -13,7 +15,8 @@ const generateSwaggerSchema = () => {
       let sout = {};
       sout[baseFilename] = eJsonSchema;
       const edata = JSON.stringify(sout, null, 2);
-      const content = `module.exports = ${edata};`; // FIXME linter issue after generate
+      const content = prettier.format(`module.exports = ${edata};`, { ...packageJson.prettier, parser: "babel" });
+
       fs.writeFileSync(path.resolve(__dirname, `../../common/model/swaggerSchema/${baseFilename}.js`), content);
     }
   });
