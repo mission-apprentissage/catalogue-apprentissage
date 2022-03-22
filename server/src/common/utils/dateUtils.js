4453f76e599c625920d359e47f1cf1df35fb6b78
Quentin Petel <contact@quentinpetel.fr>
2022-03-22 16:58:44 +0100

    Add affelnet reinitStatus job

---
+++ server/src/common/utils/dateUtils.js
@@ -0,0 +1,33 @@
+/**
+ * Retourne le premier jour du dernier mois correspondant au paramètre.
+ *
+ * @param {'01'|'02'|'03'|'04'|'05'|'06'|'07'|'08'|'09'|'10'|'11'|'12'} month
+ * @returns {Date}
+ */
+const getLastMonth = (month) => {
+  const today = new Date();
+  if (month < 1 || month > 12) return undefined;
+  const year = month <= today.getMonth() ? today.getFullYear() : today.getFullYear() - 1;
+  return new Date(`${year}-${month}-01`);
+};
+
+/**
+ * Compare deux dates et vérifie qu'elles sont le même jour (indépendamment de l'heure)
+ *
+ * @param {Date} date1
+ * @param {Date} date2
+ * @return {boolean}
+ */
+const isSameDate = (date1, date2) => {
+  if (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+module.exports = { isSameDate, getLastMonth };
---
+++ server/src/jobs/affelnet/reinitStatus/controller.js
@@ -0,0 +1,95 @@
+const { Formation } = require("../../../common/model");
+const logger = require("../../../common/logger");
+const { AFFELNET_STATUS } = require("../../../constants/status");
+
+/**
+ * Si l'historique conteint des affelnet_statut différent de 'en attante de publication' après la date, cela signifie que la formation n'est pas 'en attente de publication' depuis la date spécifiée
+ *
+ * @param {*} formation
+ * @param {Date} date
+ * @returns
+ */
+const allHistoryIsEnAttenteAfterDate = (formation, date) => {
+  return (
+    formation.updates_history
+      ?.filter((history) => history.updated_at >= date.getTime())
+      ?.filter((history) => history.to?.affelnet_statut && history.to?.affelnet_statut !== AFFELNET_STATUS.EN_ATTENTE)
+      ?.length === 0
+  );
+};
+
+/**
+ * Si l'historique contenant une modification de affelnet_statut juste avant la date est différente de 'en attente de publication', cela signifie qu'il y a eu un changement de status après la date spécifiée
+ *
+ * @param {*} formation
+ * @param {Date} date
+ * @returns
+ */
+const lastHistoryIsEnAttenteBeforeDate = (formation, date) =>
+  formation.updates_history?.filter((history) => history.updated_at < date.getTime())?.reverse()[0]?.to
+    ?.affelnet_statut === AFFELNET_STATUS.EN_ATTENTE;
+
+/**
+ *
+ * @param {Date} date La date à partir de laquelle les formations ayant été passées au statut Affelnet 'en attente de publication' vont repassé à hors périmètre.
+ */
+const run = async (date) => {
+  let count = 0;
+
+  const query = {
+    published: true,
+    affelnet_statut: AFFELNET_STATUS.EN_ATTENTE,
+    // affelnet_statut_history: {
+    //   $exists: true,
+    //   $ne: [],
+    // },
+  };
+
+  const totalBefore = await Formation.countDocuments(query);
+
+  console.info(`> ${totalBefore} formations '${AFFELNET_STATUS.EN_ATTENTE}' sur Affelnet`);
+
+  const cursor = await Formation.find(query).cursor();
+
+  for await (const formation of cursor) {
+    if (allHistoryIsEnAttenteAfterDate(formation, date) && lastHistoryIsEnAttenteBeforeDate(formation, date)) {
+      try {
+        console.error(formation._id);
+        await Formation.updateOne(
+          { _id: formation._id },
+          {
+            $set: {
+              affelnet_statut: AFFELNET_STATUS.HORS_PERIMETRE,
+            },
+            $push: {
+              affelnet_statut_history: { affelnet_statut: AFFELNET_STATUS.HORS_PERIMETRE, date: new Date() },
+              updates_history: {
+                from: { affelnet_statut: formation.affelnet_statut },
+                to: { affelnet_statut: AFFELNET_STATUS.HORS_PERIMETRE },
+                updated_at: new Date(),
+              },
+            },
+          }
+        );
+
+        if (++count % 10 === 0) {
+          console.info(`> ${count} status Affelnet mis à jour.`);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  console.info(`> ${count} status Affelnet mis à jour.`);
+
+  const totalAfter = await Formation.countDocuments(query);
+
+  logger.info(`Total formations réinitialisées dans le catalogue : ${totalAfter}/${totalBefore}\n`);
+};
+
+module.exports = {
+  run,
+  allHistoryIsEnAttenteAfterDate,
+  lastHistoryIsEnAttenteBeforeDate,
+};
