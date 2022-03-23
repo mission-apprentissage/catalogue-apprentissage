125234fbd52a01df95e3b885846dbac50180ce05
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-03-23 17:04:50 +0100

    feat: Script de réinitialisation des statuts Affelnet (#588)
    * Add affelnet reinitStatus job
    
    * Add afReinitStatus on jobs/index
    
    * Fix MR & add tests
    
    * fix MR
    
    * Fix MR

---
+++ server/tests/unit/common/utils/dateUtils.test.js
@@ -0,0 +1,32 @@
+const assert = require("assert");
+const { getLastMonth, isSameDate } = require("../../../../src/common/utils/dateUtils");
+
+describe(__filename, () => {
+  describe("getLastMonth", () => {
+    it("should work as expected", () => {
+      const now = new Date();
+      const month = "09";
+      const testSeptembre = getLastMonth(month);
+
+      const lastSeptembre =
+        now.getMonth() + 1 >= +month
+          ? new Date(`${now.getFullYear()}-09-01`)
+          : new Date(`${now.getFullYear() - 1}-09-01`);
+
+      assert.strictEqual(testSeptembre.getTime(), lastSeptembre.getTime());
+    });
+  });
+
+  describe("isSameDate", () => {
+    it("should work as expected", () => {
+      const date1 = new Date(1648049758847);
+      const date2 = new Date(1648049758849);
+      const date3 = new Date(1699046758849);
+
+      assert.strictEqual(isSameDate(date1, date2), true);
+      assert.strictEqual(isSameDate(date2, date1), true);
+      assert.strictEqual(isSameDate(date1, date3), false);
+      assert.strictEqual(isSameDate(date2, date3), false);
+    });
+  });
+});
