b8566ee74fa5aa5c7d4c89ff8de36abcdf10fddc
Quentin Petel <contact@quentinpetel.fr>
2022-04-08 10:14:33 +0200

    fix: missing breadcrumb 'Mes Actions expertes'

--- ui/src/pages/perimetre/Perimetre.jsx
+++ ui/src/pages/perimetre/Perimetre.jsx
@@ -246,7 +246,13 @@ export default ({ plateforme }) => {
     <Layout>
       <Box w="100%" pt={[4, 8]} px={[1, 1, 12, 24]}>
         <Container maxW="xl">
-          <Breadcrumb pages={[{ title: "Accueil", to: "/" }, { title: title }]} />
+          <Breadcrumb
+            pages={[
+              { title: "Accueil", to: "/" },
+              { title: "Mes actions expertes", to: "/mes-actions" },
+              { title: title },
+            ]}
+          />
           <Heading textStyle="h2" color="grey.800" mt={5} pb={4}>
             {title}
           </Heading>
