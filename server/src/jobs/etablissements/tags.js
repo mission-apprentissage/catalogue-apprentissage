7e0ab1a779ae1ac0ba6cb52127768c8b3e7d78d6
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-04-28 09:32:32 +0200

    feat: Réinitialisation des tags établissements & dépublication des organismes sans formations dans le flux (#647)
    * feat: Réinitialisation des tags établissements & dépublication des organismes sans formations dans le flux
    
    * DONE
    
    * Fix tests
    
    * fix PR
    
    * Add sort
    
    * Update server/src/jobs/etablissements/tags.js
    
    Co-authored-by: Eric Plaquevent <eric.plaquevent@gmail.com>

---
+++ server/src/jobs/etablissements/tags.js
@@ -0,0 +1,52 @@
+const { runScript } = require("../scriptWrapper");
+const logger = require("../../common/logger");
+const { Etablissement, Formation } = require("../../common/model");
+
+const etablissementTags = async () => {
+  logger.info("Settings tags on 'etablissements'...");
+
+  const cursor = Etablissement.find({}).cursor();
+  let count = 0,
+    updated = 0,
+    deleted = 0;
+  for await (const etablissement of cursor) {
+    (count++ + 1) % 100 === 0 && console.log(count);
+    try {
+      const tags = await Formation.distinct("tags", {
+        published: true,
+        $or: [
+          { etablissement_gestionnaire_id: etablissement._id },
+          { etablissement_formateur_id: etablissement._id },
+          { etablissement_responsable_id: etablissement._id },
+        ],
+      }).sort();
+
+      const diff = tags
+        .filter((tag) => !etablissement?.tags?.includes(tag))
+        .concat(etablissement?.tags?.filter((tag) => !tags.includes(tag)));
+
+      !tags.length &&
+        deleted++ &&
+        (await Etablissement.findOneAndUpdate({ _id: etablissement._id }, { published: false, tags })) &&
+        console.warn(`Unpublishing ${etablissement._id}`);
+
+      tags.length &&
+        diff.length &&
+        updated++ &&
+        (await Etablissement.findOneAndUpdate({ _id: etablissement._id }, { tags })) &&
+        console.info(`Updating ${etablissement._id}`);
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+
+  logger.info(`Settings tags on 'etablissements' : 🆗 // ${updated} updated // ${deleted} deleted`);
+};
+
+module.exports = etablissementTags;
+
+if (process.env.standalone) {
+  runScript(async () => {
+    await etablissementTags();
+  });
+}
