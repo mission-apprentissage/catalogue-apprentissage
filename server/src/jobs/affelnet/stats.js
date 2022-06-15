cf46f3030298a2f31bb49213731f590bb22151bb
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-06-15 21:02:34 +0200

    feat: Console de pilotage (suivi dans le temps par académie) (#677)
    * feat: Console de pilotage (suivi dans le temps par académie)
    
    * new schema for unwind

--- server/src/jobs/affelnet/stats.js
+++ server/src/jobs/affelnet/stats.js
@@ -2,18 +2,25 @@ const { runScript } = require("../scriptWrapper");
 
 const { AFFELNET_STATUS } = require("../../constants/status");
 const { ConsoleStat, Formation } = require("../../common/model");
+const { academies } = require("../../constants/academies");
 
-const stats = async () => {
-  console.log(`--- Calcul des statistiques Affelnet ---`);
+const computeStats = async (academie = undefined) => {
+  const scopeFilter = academie ? { nom_academie: academie } : {};
+  // const scopeLog = academie ? `[${academie}]` : `[global]`;
 
-  const filterPublie = { affelnet_statut: AFFELNET_STATUS.PUBLIE };
+  const filterPublie = {
+    affelnet_statut: AFFELNET_STATUS.PUBLIE,
+    ...scopeFilter,
+  };
   const filterIntegrable = {
     affelnet_statut: { $ne: AFFELNET_STATUS.HORS_PERIMETRE },
+    ...scopeFilter,
   };
+
   const formations_publiees = await Formation.countDocuments(filterPublie);
-  console.log(`Formations publiées : ${formations_publiees}`);
+  // console.log(`${scopeLog} Formations publiées : ${formations_publiees}`);
   const formations_integrables = await Formation.countDocuments(filterIntegrable);
-  console.log(`Formations intégrables : ${formations_integrables}`);
+  // console.log(`${scopeLog} Formations intégrables : ${formations_integrables}`);
 
   const organismes_gestionnaires_avec_formations_publiees = await Formation.distinct(
     "etablissement_gestionnaire_id",
@@ -31,7 +38,7 @@ const stats = async () => {
     ]),
   ].length;
 
-  console.log(`Organismes avec formations publiées : ${organismes_avec_formations_publiees}`);
+  // console.log(`${scopeLog} Organismes avec formations publiées : ${organismes_avec_formations_publiees}`);
 
   const organismes_gestionnaires_avec_formations_integrables = await Formation.distinct(
     "etablissement_gestionnaire_id",
@@ -49,15 +56,40 @@ const stats = async () => {
     ]),
   ].length;
 
-  console.log(`Organismes avec formations intégrables : ${organismes_avec_formations_integrables}`);
+  // console.log(`${scopeLog} Organismes avec formations intégrables : ${organismes_avec_formations_integrables}`);
 
-  await ConsoleStat.create({
-    plateforme: "affelnet",
+  const details = (
+    await Promise.all(
+      Object.values(AFFELNET_STATUS).flatMap(async (value) => ({
+        [value]: await Formation.countDocuments({ affelnet_statut: value, ...scopeFilter }),
+      }))
+    )
+  ).reduce(function (result, current) {
+    return Object.assign(result, current);
+  }, {});
+
+  return {
+    academie,
     formations_publiees,
     formations_integrables,
     organismes_avec_formations_publiees,
     organismes_avec_formations_integrables,
-  });
+    details,
+  };
+};
+
+const stats = async () => {
+  console.log(`--- Calcul des statistiques Affelnet ---`);
+
+  const stat = {
+    plateforme: "affelnet",
+    stats: await Promise.all([
+      { stats: await computeStats(), academie: "Toute la France" },
+      ...Object.values(academies).map(async (academie) => await computeStats(academie.nom_academie)),
+    ]),
+  };
+
+  return await ConsoleStat.create(stat);
 };
 
 module.exports = { stats };
--- server/src/jobs/parcoursup/stats.js
+++ server/src/jobs/parcoursup/stats.js
@@ -2,18 +2,25 @@ const { runScript } = require("../scriptWrapper");
 
 const { PARCOURSUP_STATUS } = require("../../constants/status");
 const { ConsoleStat, Formation } = require("../../common/model");
+const { academies } = require("../../constants/academies");
 
-const stats = async () => {
-  console.log(`--- Calcul des statistiques Parcoursup ---`);
+const computeStats = async (academie = undefined) => {
+  const scopeFilter = academie ? { nom_academie: academie } : {};
+  // const scopeLog = academie ? `[${academie}]` : `[global]`;
 
-  const filterPublie = { parcoursup_statut: PARCOURSUP_STATUS.PUBLIE };
+  const filterPublie = {
+    parcoursup_statut: PARCOURSUP_STATUS.PUBLIE,
+    ...scopeFilter,
+  };
   const filterIntegrable = {
     parcoursup_statut: { $ne: PARCOURSUP_STATUS.HORS_PERIMETRE },
+    ...scopeFilter,
   };
+
   const formations_publiees = await Formation.countDocuments(filterPublie);
-  console.log(`Formations publiées : ${formations_publiees}`);
+  // console.log(`${scopeLog} Formations publiées : ${formations_publiees}`);
   const formations_integrables = await Formation.countDocuments(filterIntegrable);
-  console.log(`Formations intégrables : ${formations_integrables}`);
+  // console.log(`${scopeLog} Formations intégrables : ${formations_integrables}`);
 
   const organismes_gestionnaires_avec_formations_publiees = await Formation.distinct(
     "etablissement_gestionnaire_id",
@@ -31,7 +38,7 @@ const stats = async () => {
     ]),
   ].length;
 
-  console.log(`Organismes avec formations publiées : ${organismes_avec_formations_publiees}`);
+  // console.log(`${scopeLog} Organismes avec formations publiées : ${organismes_avec_formations_publiees}`);
 
   const organismes_gestionnaires_avec_formations_integrables = await Formation.distinct(
     "etablissement_gestionnaire_id",
@@ -49,15 +56,40 @@ const stats = async () => {
     ]),
   ].length;
 
-  console.log(`Organismes avec formations intégrables : ${organismes_avec_formations_integrables}`);
+  // console.log(`${scopeLog} Organismes avec formations intégrables : ${organismes_avec_formations_integrables}`);
 
-  await ConsoleStat.create({
-    plateforme: "parcoursup",
+  const details = (
+    await Promise.all(
+      Object.values(PARCOURSUP_STATUS).flatMap(async (value) => ({
+        [value]: await Formation.countDocuments({ parcoursup_statut: value, ...scopeFilter }),
+      }))
+    )
+  ).reduce(function (result, current) {
+    return Object.assign(result, current);
+  }, {});
+
+  return {
+    academie,
     formations_publiees,
     formations_integrables,
     organismes_avec_formations_publiees,
     organismes_avec_formations_integrables,
-  });
+    details,
+  };
+};
+
+const stats = async () => {
+  console.log(`--- Calcul des statistiques Parcoursup ---`);
+
+  const stat = {
+    plateforme: "parcoursup",
+    stats: await Promise.all([
+      { stats: await computeStats(), academie: "Toute la France" },
+      ...Object.values(academies).map(async (academie) => await computeStats(academie.nom_academie)),
+    ]),
+  };
+
+  return await ConsoleStat.create(stat);
 };
 
 module.exports = { stats };
