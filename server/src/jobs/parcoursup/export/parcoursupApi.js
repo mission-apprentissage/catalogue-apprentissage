8ae59294e59ab57e704c2f60f9d1dde2334c1d33
Quentin Petel <contact@quentinpetel.fr>
2022-06-26 23:00:27 +0200

    fix: Parcoursup key from environment without space

--- server/src/jobs/parcoursup/export/parcoursupApi.js
+++ server/src/jobs/parcoursup/export/parcoursupApi.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { createParcoursupToken } = require("../../../common/utils/jwtUtils");
 
 const endpoint = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_ENDPOINT;
-const privateKey = (process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_PRIVATE_KEY ?? "").replace(/\n +/gm, "\n");
+const privateKey = (process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_PRIVATE_KEY ?? "").replace(/\n */gm, "\n");
 const pwd = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_PRIVATE_KEY_PWD;
 const id = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_CERTIFICATE_ID;
 
