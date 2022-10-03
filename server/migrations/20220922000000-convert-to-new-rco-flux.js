e2bfd4323d96ef7fe3ead1f4cab5bb64270da855
Quentin Petel <contact@quentinpetel.fr>
2022-10-03 11:26:42 +0200

    fix migration

--- server/migrations/20220922000000-convert-to-new-rco-flux.js
+++ server/migrations/20220922000000-convert-to-new-rco-flux.js
@@ -1,13 +1,12 @@
 module.exports = {
   async up(db) {
-    await db.collection("rcoFormations").drop();
+    await db.collection("rcoformations").drop();
     await db.collection("conventionfiles").drop();
     await db.collection("bcnnmefs").drop();
-    await db.collection("bcnndispositifformations").drop();
-    await db.collection("bcnnniveauformationdiplomes").drop();
     await db.collection("bcnformationdiplomes").drop();
     await db.collection("bcnlettrespecialites").drop();
-    await db.collection("bcnmefs").drop();
+    await db.collection("bcnndispositifformations").drop();
+    await db.collection("bcnnniveauformationdiplomes").drop();
     await db.collection("ficherncps").drop();
     await db.collection("oniseps").drop();
     await db.collection("sandboxformations").drop();
