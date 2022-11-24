cf60a445d36a9995f826e2233900c4e7731eab1a
Quentin Petel <contact@quentinpetel.fr>
2022-11-24 12:29:06 +0100

    fix: envoi cfd

--- server/src/jobs/parcoursup/clean/sendWs.js
+++ server/src/jobs/parcoursup/clean/sendWs.js
@@ -2,25 +2,24 @@ const { Formation } = require("../../../common/model");
 const logger = require("../../../common/logger");
 const { runScript } = require("../../scriptWrapper");
 const { cursor } = require("../../../common/utils/cursor");
-// const { delay } = require("../../../common/utils/asyncUtils");
 const { updateFormation } = require("../export/parcoursupApi");
 const ObjectsToCsv = require("objects-to-csv");
 
-const run = async ({ limit, skip, file } = { limit: 0, skip: 0 }) => {
+const run = async ({ limit, skip, file, clean } = { limit: 0, skip: 0, clean: false }) => {
   const results = [];
   try {
     await cursor(
       Formation.find({ parcoursup_id: { $ne: undefined } })
         .limit(limit)
         .skip(skip),
-      async ({ cle_ministere_educatif, parcoursup_id, parcoursup_mefs_10, rncp_code, cfd_entree, uai_formation }) => {
+      async ({ cle_ministere_educatif, parcoursup_id, parcoursup_mefs_10, rncp_code, cfd, uai_formation }) => {
         const [{ mef10: mef } = { mef10: null }] = parcoursup_mefs_10 ?? [];
 
         const toSend = {
           user: null,
           g_ta_cod: Number(parcoursup_id),
           rncp: rncp_code ? [Number(rncp_code.replace("RNCP", ""))] : [],
-          cfd: cfd_entree,
+          cfd,
           uai: uai_formation,
           rco: cle_ministere_educatif,
           mef: mef ? Number(mef) : null,
@@ -41,6 +40,11 @@ const run = async ({ limit, skip, file } = { limit: 0, skip: 0 }) => {
             ...toSend,
             error: { msg: err.response.data, status: err.response.status },
           };
+
+          if (clean) {
+            /* On dés-associe les formations PSUP et catalogue en cas d'erreur du WS */
+            await Formation.updateOne({ cle_ministere_educatif }, { parcoursup_id: null });
+          }
         }
 
         console.log(results[index]);
@@ -52,17 +56,15 @@ const run = async ({ limit, skip, file } = { limit: 0, skip: 0 }) => {
 
   logger.info(results);
 
-  (async () => {
-    const csv = new ObjectsToCsv(results);
+  const csv = new ObjectsToCsv(results);
 
-    // Return the CSV file as string:
-    console.log(await csv.toString());
+  // Return the CSV file as string:
+  console.log(await csv.toString());
 
-    if (file) {
-      // Save to file:
-      await csv.toDisk(file);
-    }
-  })();
+  if (file) {
+    // Save to file:
+    await csv.toDisk(file);
+  }
 };
 
 if (process.env.standalone) {
@@ -70,11 +72,16 @@ if (process.env.standalone) {
     logger.info(" -- Start formation parcoursup_id sendWs -- ");
     const args = process.argv.slice(2);
 
+    // Limite de formation envoyée sur le WS de maj Parcoursup
     const limit = args.find((arg) => arg.startsWith("--limit"))?.split("=")?.[1];
+    // Nombre de formation à skip avant d'appliquer la limite
     const skip = args.find((arg) => arg.startsWith("--skip"))?.split("=")?.[1];
+    // Nom du fichier csv dans lequel on écrira les résultats
     const file = args.find((arg) => arg.startsWith("--file"))?.split("=")?.[1];
+    // Option pour dés-associer les parcoursup_id en cas d'erreur du WS (à n'utiliser qu'une fois les cas d'erreur analysés)
+    const clean = !!args.find((arg) => arg.startsWith("--clean"));
 
-    await run({ limit: limit ? Number(limit) : 0, skip: skip ? Number(skip) : 0, file });
+    await run({ limit: limit ? Number(limit) : 0, skip: skip ? Number(skip) : 0, file, clean });
 
     logger.info(" -- End formation parcoursup_id sendWs -- ");
   });
--- server/src/jobs/parcoursup/export/parcoursupApi.js
+++ server/src/jobs/parcoursup/export/parcoursupApi.js
@@ -6,7 +6,6 @@ const axios = require("axios");
 const logger = require("../../../common/logger");
 const { createParcoursupToken } = require("../../../common/utils/jwtUtils");
 
-const endpoint = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_ENDPOINT;
 const privateKey = (process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_PRIVATE_KEY ?? "").replace(/\n */gm, "\n");
 const pwd = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_PRIVATE_KEY_PWD;
 const id = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_CERTIFICATE_ID;
@@ -15,6 +14,7 @@ const id = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_CERTIFICATE_ID;
  * POST a formation to Parcoursup WS
  */
 const postFormation = async (data) => {
+  const endpoint = process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_ENDPOINT + "/ApiFormation/api/formations";
   const token = createParcoursupToken({ data, privateKey, pwd, id });
 
   try {
@@ -33,16 +33,14 @@ const postFormation = async (data) => {
  * POST a formation to Parcoursup WS
  */
 const updateFormation = async (data) => {
+  const endpoint =
+    process.env.CATALOGUE_APPRENTISSAGE_PARCOURSUP_ENDPOINT + "/ApiFormation/api/formations/majFormation";
   const token = createParcoursupToken({ data, privateKey, pwd, id });
 
   try {
-    const { data: responseData } = await axios.post(
-      "https://test2.parcoursup.fr/ApiFormation/api/formations/majFormation",
-      data,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
+    const { data: responseData } = await axios.post(endpoint, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
     return responseData;
   } catch (err) {
