8d2d8f5406a3e529be31fb31865b4ca19a1e4b73
ljaymot <luc.jaymot@intercariforef.org>
2022-12-02 16:12:58 +0100

    Année badge test

--- ui/src/common/components/formation/DescriptionBlock.jsx
+++ ui/src/common/components/formation/DescriptionBlock.jsx
@@ -142,25 +142,7 @@ export const DescriptionBlock = ({ formation }) => {
             </Text>
           </DureeContainer>
 
-          {formation.annee === "X" && (
-            <Box
-              bg={"orangesoft.200"}
-              p={4}
-              mb={formation.annee_incoherente ? 0 : 4}
-              borderLeft={"4px solid"}
-              borderColor={"orangesoft.500"}
-              w={"full"}
-            >
-              <Text>
-                Année d'entrée en apprentissage :{" "}
-                <Text as="span" variant="highlight" bg={"transparent"}>
-                  <DureeAnnee value={formation.annee} />
-                </Text>{" "}
-                <InfoTooltip description={helpText.formation.annee} />
-              </Text>
-            </Box>
-          )}
-           {formation.annee === "Y" && (
+          {formation.annee === "X"  && (
             <Box
               bg={"orangesoft.200"}
               p={4}
