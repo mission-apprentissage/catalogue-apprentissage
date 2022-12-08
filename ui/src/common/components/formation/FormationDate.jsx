b0da77f5b18585372114ac5a1f101168df2a40c9
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-12-08 10:51:39 +0100

    feat: Ajout des dates de session (date_debut / date_fin / modalites_entrees_sorties) (#729)

---
+++ ui/src/common/components/formation/FormationDate.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import { Text } from "@chakra-ui/react";
+
+export const FormationDate = ({ formation }) => {
+  const dates = formation.date_debut?.map((date_debut, index) => ({
+    date_debut,
+    date_fin: formation.date_fin ? formation.date_fin[index] : null,
+    modalites_entrees_sorties: formation.modalites_entrees_sorties ? formation.modalites_entrees_sorties[index] : null,
+  }));
+
+  return (
+    <Text variant="highlight" as="span">
+      {dates?.map(({ date_debut, date_fin, modalites_entrees_sorties }) => (
+        <>
+          Du {new Date(date_debut).toLocaleDateString("fr-FR")} au {new Date(date_fin).toLocaleDateString("fr-FR")}.{" "}
+          {modalites_entrees_sorties && "Entrée-sortie permanente."}
+          <br />
+        </>
+      ))}
+    </Text>
+  );
+};
