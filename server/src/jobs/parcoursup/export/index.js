59e5f6b7915585059106ab3019eaafff6b025e8a
Quentin Petel <contact@quentinpetel.fr>
2023-01-05 15:35:00 +0100

    Handle parcoursup_statut_history when setting parcoursup_staut from front

--- server/src/jobs/parcoursup/export/index.js
+++ server/src/jobs/parcoursup/export/index.js
@@ -121,12 +121,14 @@ const createFormation = async (formation, email = null) => {
     await formation.save({ validateBeforeSave: false });
   } catch (e) {
     logger.error("Parcoursup WS error", e?.response?.status, e?.response?.data ?? e, data);
+    formation.last_update_at = Date.now();
     formation.last_update_who = `web service Parcoursup${email ? `, sent by ${email}` : ""}, received error`;
     formation.parcoursup_error = `${e?.response?.status} ${
       e?.response?.data?.message ?? e?.response?.data ?? "erreur de création"
     }`;
 
     if (getParcoursupError(formation)) {
+      formation.last_statut_update_date = new Date();
       formation.parcoursup_statut = PARCOURSUP_STATUS.REJETE;
       formation.rejection = {
         error: formation.parcoursup_error,
