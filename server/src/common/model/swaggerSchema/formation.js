d41fe3a88798012f2d5cdf62e8cb661789e6974d
Quentin Petel <contact@quentinpetel.fr>
2023-01-30 14:03:17 +0100

    fix

--- server/src/common/model/swaggerSchema/formation.js
+++ server/src/common/model/swaggerSchema/formation.js
@@ -372,7 +372,7 @@ module.exports = {
             },
             updated_at: {
               type: "string",
-              default: "2023-01-30T12:49:28.142Z",
+              default: "2023-01-30T12:57:53.653Z",
               description: "Date de la mise à jour",
               format: "date-time",
             },
