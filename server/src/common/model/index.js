a046be8237e5b2c6f05fed5b54630a5b14a821da
Quentin Petel <contact@quentinpetel.fr>
2023-01-30 12:30:11 +0100

    feat: Ajout d'un historique global des modifications pour formations et etablissements

--- server/src/common/model/index.js
+++ server/src/common/model/index.js
@@ -1,6 +1,7 @@
+const diffHistory = require("mongoose-diff-history/diffHistory");
 const { mongoose } = require("../mongodb");
 const { mongoosastic, getElasticInstance } = require("../esClient");
-const schema = require("../model/schema");
+const schemas = require("../model/schema");
 
 const createModel = (modelName, [schemaDescriptor, schemaOptions], options = {}) => {
   if (mongoose.models[modelName]) {
@@ -16,39 +17,49 @@ const createModel = (modelName, [schemaDescriptor, schemaOptions], options = {})
     options.createMongoDBIndexes(schema);
   }
 
+  if (options.diff) {
+    schema.plugin(diffHistory.plugin, options.diff);
+  }
+
   return mongoose.model(modelName, schema, options.collectionName);
 };
 
 module.exports = {
-  User: createModel("user", schema.get("user")),
-  Role: createModel("role", schema.get("role")),
-  Formation: createModel("formation", schema.get("formation"), {
+  User: createModel("user", schemas.get("user")),
+  Role: createModel("role", schemas.get("role")),
+  Formation: createModel("formation", schemas.get("formation"), {
     esIndexName: "formation",
     filter: (doc) => {
       return !doc.published;
     },
+    diff: {
+      omit: ["updates_history", "updated_at"],
+    },
   }),
-  Report: createModel("report", schema.get("report")),
-  Log: createModel("log", schema.get("log")),
-  AffelnetFormation: createModel("affelnetformation", schema.get("affelnetFormation")),
-  Etablissement: createModel("etablissement", schema.get("etablissement"), {
+  Report: createModel("report", schemas.get("report")),
+  Log: createModel("log", schemas.get("log")),
+  AffelnetFormation: createModel("affelnetformation", schemas.get("affelnetFormation")),
+  Etablissement: createModel("etablissement", schemas.get("etablissement"), {
     esIndexName: "etablissements",
     filter: (doc) => {
       return !doc.published;
     },
+    diff: {
+      omit: ["updates_history", "updated_at"],
+    },
   }),
-  ParcoursupFormation: createModel("parcoursupformations", schema.get("parcoursupFormation")),
-  ParcoursupFormationCheck: createModel("parcoursupformationchecks", schema.get("parcoursupFormationCheck")),
-  SandboxFormation: createModel("sandboxformation", schema.get("formation")),
-  Statistique: createModel("statistique", schema.get("statistique")),
-  Alert: createModel("alert", schema.get("alert")),
-  ReglePerimetre: createModel("regleperimetre", schema.get("reglePerimetre")),
-  Consumption: createModel("consumption", schema.get("consumption")),
-  DualControlEtablissement: createModel("dualcontroletablissement", schema.get("dualControlEtablissement")),
-  DualControlFormation: createModel("dualcontrolformation", schema.get("dualControlFormation")),
-  DualControlReport: createModel("dualcontrolreport", schema.get("dualControlReport")),
-  DualControlPerimeterReport: createModel("dualcontrolperimeterreport", schema.get("dualControlPerimeterReport")),
-  ConsoleStat: createModel("consolestat", schema.get("consoleStat")),
-  PreviousSeasonFormation: createModel("previousSeasonFormation", schema.get("previousSeasonFormation")),
-  PreviousSeasonFormationStat: createModel("previousSeasonFormationStat", schema.get("previousSeasonFormationStat")),
+  ParcoursupFormation: createModel("parcoursupformations", schemas.get("parcoursupFormation")),
+  ParcoursupFormationCheck: createModel("parcoursupformationchecks", schemas.get("parcoursupFormationCheck")),
+  SandboxFormation: createModel("sandboxformation", schemas.get("formation")),
+  Statistique: createModel("statistique", schemas.get("statistique")),
+  Alert: createModel("alert", schemas.get("alert")),
+  ReglePerimetre: createModel("regleperimetre", schemas.get("reglePerimetre")),
+  Consumption: createModel("consumption", schemas.get("consumption")),
+  DualControlEtablissement: createModel("dualcontroletablissement", schemas.get("dualControlEtablissement")),
+  DualControlFormation: createModel("dualcontrolformation", schemas.get("dualControlFormation")),
+  DualControlReport: createModel("dualcontrolreport", schemas.get("dualControlReport")),
+  DualControlPerimeterReport: createModel("dualcontrolperimeterreport", schemas.get("dualControlPerimeterReport")),
+  ConsoleStat: createModel("consolestat", schemas.get("consoleStat")),
+  PreviousSeasonFormation: createModel("previousSeasonFormation", schemas.get("previousSeasonFormation")),
+  PreviousSeasonFormationStat: createModel("previousSeasonFormationStat", schemas.get("previousSeasonFormationStat")),
 };
