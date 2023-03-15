96d3086736a0a1520a5840cf895ae19f97ca20e5
Quentin Petel <contact@quentinpetel.fr>
2023-03-15 11:46:12 +0100

    fix: Correction du problème de droit pour l'upload de fichiers

--- server/src/common/utils/rolesUtils.js
+++ server/src/common/utils/rolesUtils.js
@@ -2,7 +2,7 @@ const hasOneOfRoles = (user, roles) => {
   return !!user.roles?.map((role) => roles.includes(role)).length;
 };
 
-const isUserAdmin = (user) => user && user.permissions && user.permissions.isAdmin;
+const isUserAdmin = (user) => !!user?.isAdmin;
 
 const hasAccessTo = (user, aclRef) => {
   return isUserAdmin(user) || user.acl?.includes(aclRef);
