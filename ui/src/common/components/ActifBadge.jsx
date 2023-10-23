63e9fe392c3333efa41f1ccf50a2029fd1135eb6
ljaymot <luc.jaymot@intercariforef.org>
2023-10-23 15:37:00 +0200

    Badge actif

---
+++ ui/src/common/components/ActifBadge.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Badge, Flex, Text } from "@chakra-ui/react";
+import { CheckLine, Question } from "../../theme/components/icons";
+
+export const ActifBadge = ({ value, ...props }) => {
+    let text;
+    let variant;
+    let Icon;
+
+    switch (value) {
+        case "actif":
+            text = "SIRET actif";
+            variant = "ok";
+            Icon = CheckLine;
+        break;
+
+        case "inactif":
+            text = "SIRET inactif";
+            variant = "notOk";
+            Icon = null;
+        break;
+    }
+
+    return (
+        <Badge variant={variant} {...props}>
+            <Flex alignItems="center">
+                <Text mx={1} as={"span"}>
+                    {text}
+                </Text>
+                {Icon && <Icon ml={1} />}
+            </Flex>
+        </Badge>
+    );
+};
