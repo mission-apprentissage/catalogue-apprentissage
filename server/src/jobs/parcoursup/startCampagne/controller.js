1395716fe28628cd75ff45656597a587207c0cb4
Quentin Petel <contact@quentinpetel.fr>
2023-11-24 17:19:03 +0100

    fix: Vérification du périmètre lors de la réinitialisation de la campagne PARCOURSUP

--- server/src/jobs/parcoursup/startCampagne/controller.js
+++ server/src/jobs/parcoursup/startCampagne/controller.js
@@ -2,24 +2,26 @@ const { Formation } = require("../../../common/model");
 const logger = require("../../../common/logger");
 const { PARCOURSUP_STATUS } = require("../../../constants/status");
 const { cursor } = require("../../../common/utils/cursor");
-const { isBetween } = require("../../../common/utils/dateUtils");
-const { getSessionStartDate, getSessionEndDate } = require("../../../common/utils/rulesUtils");
 
 const run = async () => {
   let updated = 0;
-  const sessionStartDate = getSessionStartDate();
-  const sessionEndDate = getSessionEndDate();
 
   await cursor(
     Formation.find({ parcoursup_statut: { $ne: PARCOURSUP_STATUS.NON_PUBLIABLE_EN_LETAT } }),
-    async ({ _id, parcoursup_id, parcoursup_statut, parcoursup_statut_history, date_debut }) => {
+    async ({
+      _id,
+      parcoursup_id,
+      parcoursup_statut,
+      parcoursup_statut_history,
+      parcoursup_session,
+      parcoursup_perimetre,
+    }) => {
       let next_parcoursup_statut;
 
-      const in_next_session =
-        date_debut?.filter((toCheck) => isBetween(sessionStartDate, new Date(toCheck), sessionEndDate))?.length > 0;
-
-      if (parcoursup_id && parcoursup_statut === PARCOURSUP_STATUS.PUBLIE && in_next_session) {
-        next_parcoursup_statut = PARCOURSUP_STATUS.EN_ATTENTE;
+      if (parcoursup_id && parcoursup_statut === PARCOURSUP_STATUS.PUBLIE && parcoursup_perimetre) {
+        next_parcoursup_statut = parcoursup_session
+          ? PARCOURSUP_STATUS.EN_ATTENTE
+          : PARCOURSUP_STATUS.NON_PUBLIABLE_EN_LETAT;
         await Formation.updateOne(
           { _id: _id },
           {
