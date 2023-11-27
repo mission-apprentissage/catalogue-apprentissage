1eedff0acd394c4c2a18ffc05ad32c8162704d9b
ljaymot <luc.jaymot@intercariforef.org>
2023-11-27 14:37:32 +0100

    Actif badge constante évolution

--- ui/src/common/components/ActifBadge.jsx
+++ ui/src/common/components/ActifBadge.jsx
@@ -2,33 +2,31 @@ import React from "react";
 import { Badge, Flex, Text } from "@chakra-ui/react";
 import { CheckLine, Question } from "../../theme/components/icons";
 
-export const ActifBadge = ({ value, ...props }) => {
+export const ActifBadge = ({ values, ...props }) => {
   let text;
   let variant;
   let Icon;
 
-  switch (value) {
-    case "actif":
-      text = "SIRET actif";
-      variant = "ok";
-      Icon = CheckLine;
-      break;
+  const isActif = values.etablissement_gestionnaire_actif === 'actif' && values.etablissement_formateur_actif === 'actif';
 
-    case "inactif":
-      text = "SIRET inactif";
-      variant = "notOk";
-      Icon = null;
-      break;
+  if (isActif) {
+    text = "SIRET actif";
+    variant = "ok";
+    Icon = CheckLine;
+  } else {
+    text = "SIRET inactif";
+    variant = "notOk";
+    Icon = null;
   }
 
   return (
     <Badge variant={variant} {...props}>
-      <Flex alignItems="center">
-        <Text mx={1} as={"span"}>
-          {text}
-        </Text>
-        {Icon && <Icon ml={1} />}
-      </Flex>
-    </Badge>
+    <Flex alignItems="center">
+      <Text mx={1} as={"span"}>
+        {text}
+      </Text>
+      {Icon && <Icon ml={1} />}
+    </Flex>
+  </Badge>
   );
 };
