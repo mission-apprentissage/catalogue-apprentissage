4e625e7e9589aab01d211d27a3241354487a9e87
ljaymot <luc.jaymot@intercariforef.org>
2023-11-27 14:32:12 +0100

    Remise _ plat

--- ui/src/common/components/ActifBadge.jsx
+++ ui/src/common/components/ActifBadge.jsx
@@ -2,31 +2,33 @@ import React from "react";
 import { Badge, Flex, Text } from "@chakra-ui/react";
 import { CheckLine, Question } from "../../theme/components/icons";
 
-export const ActifBadge = ({ values, ...props }) => {
+export const ActifBadge = ({ value, ...props }) => {
   let text;
   let variant;
   let Icon;
 
-  const isActif = values.etablissement_gestionnaire_actif === 'actif' && values.etablissement_formateur_actif === 'actif';
+  switch (value) {
+    case "actif":
+      text = "SIRET actif";
+      variant = "ok";
+      Icon = CheckLine;
+      break;
 
-  if (isActif) {
-    text = "SIRET actif";
-    variant = "ok";
-    Icon = CheckLine;
-  } else {
-    text = "SIRET inactif";
-    variant = "notOk";
-    Icon = null;
+    case "inactif":
+      text = "SIRET inactif";
+      variant = "notOk";
+      Icon = null;
+      break;
   }
 
   return (
     <Badge variant={variant} {...props}>
-    <Flex alignItems="center">
-      <Text mx={1} as={"span"}>
-        {text}
-      </Text>
-      {Icon && <Icon ml={1} />}
-    </Flex>
-  </Badge>
+      <Flex alignItems="center">
+        <Text mx={1} as={"span"}>
+          {text}
+        </Text>
+        {Icon && <Icon ml={1} />}
+      </Flex>
+    </Badge>
   );
 };
