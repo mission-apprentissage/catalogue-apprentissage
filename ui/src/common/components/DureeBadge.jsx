710751c46a554d4206cae5d457c7b7bd102e8097
ljaymot <luc.jaymot@intercariforef.org>
2023-12-18 15:21:34 +0100

    nsx perttier

--- ui/src/common/components/DureeBadge.jsx
+++ ui/src/common/components/DureeBadge.jsx
@@ -3,24 +3,24 @@ import { Badge, Flex, Text } from "@chakra-ui/react";
 import { CheckLine, Question } from "../../theme/components/icons";
 
 export const DureeBadge = ({ value, ...props }) => {
-    let text;
-    let variant;
-    let Icon;
+  let text;
+  let variant;
+  let Icon;
 
-    if (value === "Y") {
-        text = "Dur\u00e9e";
-        variant = "ok";
-        Icon = CheckLine;
-    }
+  if (value === "Y") {
+    text = "Dur\u00e9e";
+    variant = "ok";
+    Icon = CheckLine;
+  }
 
-    return (
-        <Badge variant={variant} {...props}>
-        <Flex alignItems="center">
-            <Text mx={1} as={"span"}>
-            {text}
-            </Text>
-            {Icon && <Icon ml={1} />}
-        </Flex>
-        </Badge>
-    );
+  return (
+    <Badge variant={variant} {...props}>
+      <Flex alignItems="center">
+        <Text mx={1} as={"span"}>
+          {text}
+        </Text>
+        {Icon && <Icon ml={1} />}
+      </Flex>
+    </Badge>
+  );
 };
