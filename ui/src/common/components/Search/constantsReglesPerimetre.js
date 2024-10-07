f4c5e567b8cb5e8e54ef4910688977ff1f1150b4
Quentin Petel <contact@quentinpetel.fr>
2024-10-07 17:06:22 +0200

    feat: Ajout du critère additionnel 'Intitulé de la formation' sur le module de périmètre

--- ui/src/common/components/Search/constantsReglesPerimetre.js
+++ ui/src/common/components/Search/constantsReglesPerimetre.js
@@ -1,6 +1,7 @@
 const queryBuilderField = [
-  { text: "Libelle court", value: "libelle_court" },
-  { text: "Niveau formation diplome", value: "niveau_formation_diplome" },
+  { text: "Intitulé de la formation", value: "intitule_long" },
+  { text: "L̶i̶b̶e̶l̶l̶e̶ ̶c̶o̶u̶r̶t̶", value: "libelle_court" },
+  { text: "N̶i̶v̶e̶a̶u̶ ̶f̶o̶r̶m̶a̶t̶i̶o̶n̶ ̶d̶i̶p̶l̶o̶m̶e̶", value: "niveau_formation_diplome" },
   { text: "MEF 10", value: "bcn_mefs_10.mef10" },
   { text: "Code RNCP", value: "rncp_code" },
   { text: "Groupe Spécialité", value: "rncp_details.nsf_code" },
